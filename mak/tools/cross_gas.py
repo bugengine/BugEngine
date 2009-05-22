55285122c224e3d258f8345a0289bf2a50693ef7
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2009-05-22 11:00:59 +0000

    darwin port
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@19 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- mak/tools/cross_gas.py
+++ mak/tools/cross_gas.py
@@ -14,19 +14,19 @@ def find_cross_gas(conf):
 		v = conf.env
 		if not v['AS']: v['AS'] = conf.find_program(target+'-as', var='AS', path_list=v['GCC_PATH'])
 		if not v['AS']: v['AS'] = conf.find_program(target+'-gas', var='AS', path_list=v['GCC_PATH'])
-		if not v['AS'] and target == v['GCC_NATIVE_TARGET']:
-			v['AS'] = conf.find_program('as', var='AS')
-			if not v['AS']: v['AS'] = conf.find_program('gas', var='AS')
+		if not v['AS']:
+			v['AS'] = conf.find_program('as', var='AS', path_list=v['GCC_PATH'])
+			if not v['AS']: v['AS'] = conf.find_program('gas', var='AS', path_list=v['GCC_PATH'])
 		if not v['AS']: conf.fatal('unable to find as for target %s' % target)
 
 		if not v['AR']: v['AR'] = conf.find_program(target+'-ar', var='AR', path_list=v['GCC_PATH'])
-		if not v['AR'] and target == v['GCC_NATIVE_TARGET']:
-			v['AR'] = conf.find_program('ar', var='AR')
+		if not v['AR']:
+			v['AR'] = conf.find_program('ar', var='AR', path_list=v['GCC_PATH'])
 		if not v['AR']: conf.fatal('unable to find ar for target %s' % target)
 
 		if not v['RANLIB']: v['RANLIB'] = conf.find_program(target+'-ranlib', var='RANLIB', path_list=v['GCC_PATH'])
-		if not v['RANLIB'] and target == v['GCC_NATIVE_TARGET']:
-			v['RANLIB'] = conf.find_program('ar', var='RANLIB')
+		if not v['RANLIB']:
+			v['RANLIB'] = conf.find_program('ar', var='RANLIB', path_list=v['GCC_PATH'])
 		if not v['RANLIB']: conf.fatal('unable to find ranlib for target %s' % target)
 
 	conf.check_tool('gas ar')
