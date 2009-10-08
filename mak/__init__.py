43fc387d64a08137ff3c7cdd9b78b9f3764f950d
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2009-10-08 21:13:44 +0000

    Fixed invalid name of PowerPC arch made linux builds resist to unrecognized GCC
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@107 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- mak/__init__.py
+++ mak/__init__.py
@@ -1,4 +1,4 @@
 alloptims		= [ 'debug', 'release', 'profile', 'final']
 allplatforms	= [ 'win32', 'linux', 'sunos', 'darwin', 'freebsd', 'psp', 'gp2x', 'nds', 'wii', 'ogc', 'gba' ]
-allarchs		= [ 'amd64', 'x86', 'alpha', 'arm', 'arm7', 'arm9', 'hppa', 'ia64', 'mips', 'ppc', 's390', 's390x', 'sparc' ]
+allarchs		= [ 'amd64', 'x86', 'alpha', 'arm', 'arm7', 'arm9', 'hppa', 'ia64', 'mips', 'powerpc', 's390', 's390x', 'sparc' ]
 builder			= None
