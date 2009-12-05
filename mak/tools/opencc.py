4e6c9b1037b2c0b4bd22c50a9c866754a2f897db
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2009-12-05 12:58:07 +0000

    forgiotten files
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@145 f3700e90-1ba7-11de-b56b-c9554f25afe1

---
+++ mak/tools/opencc.py
@@ -0,0 +1,12 @@
+from Configure import conftest
+
+
+@conftest
+def find_opencc(conf):
+	v['CC'] = conf.find_program('opencc', var='CC')
+	v['CC'] = conf.find_program('openCC', var='CXX')
+	conf.check_tool('gcc')
+
+detect = '''
+find_opencc
+'''
