4375bdd74f424a1974689af8ce981da755fe8278
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2010-09-02 18:39:17 +0000

    missing new file
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@287 f3700e90-1ba7-11de-b56b-c9554f25afe1

---
+++ src/engine/core/api/minitl/container/inl/iterator.inl
@@ -0,0 +1,92 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_MINITL_CONTAINER_INL_ITERATORS_INL_
+#define BE_MINITL_CONTAINER_INL_ITERATORS_INL_
+/*****************************************************************************/
+
+namespace minitl
+{
+
+namespace _
+{
+
+template< typename ITERATOR, typename ITERATOR_TYPE >
+static typename iterator_traits<ITERATOR>::difference_type distance(const ITERATOR& t1, const ITERATOR& t2, ITERATOR_TYPE type)
+{
+    typename ITERATOR::difference_type result = 0;
+    while(t1 != t2)
+    {
+        result++;
+        ++t1;
+    }
+    return result;
+}
+
+template< typename ITERATOR >
+static typename iterator_traits<ITERATOR>::difference_type distance(const ITERATOR& t1, const ITERATOR& t2, random_access_iterator_tag type)
+{
+    return t2 - t1;
+}
+
+}
+
+template< typename ITERATOR >
+typename iterator_traits<ITERATOR>::iterator_category   iterator_category(const ITERATOR& it)
+{
+    return iterator_traits<ITERATOR>::iterator_category();
+}
+
+template< typename ITERATOR >
+typename iterator_traits<ITERATOR>::difference_type distance(const ITERATOR& first, const ITERATOR& last)
+{
+    return _::distance(first, last, iterator_category(first));
+}
+
+template< typename T >
+difference_type distance(T* t1, T* t2)
+{
+    const byte *ptr1 = reinterpret_cast<const byte*>(t1);
+    const byte *ptr2 = reinterpret_cast<const byte*>(t2);
+    ptrdiff_t d = ptr2 - ptr1;
+    be_assert(d %  be_align(sizeof(T),be_alignof(T)) == 0, "distance between %p and %p is not a multiple of the size" | t1 | t2);
+    return d / be_align(sizeof(T),be_alignof(T));
+}
+
+template< typename T >
+difference_type distance(const T* t1, const T* t2)
+{
+    const byte *ptr1 = reinterpret_cast<const byte*>(t1);
+    const byte *ptr2 = reinterpret_cast<const byte*>(t2);
+    ptrdiff_t d = ptr2 - ptr1;
+    be_assert(d %  be_align(sizeof(T),be_alignof(T)) == 0, "distance between %p and %p is not a multiple of the size" | t1 | t2);
+    return d / be_align(sizeof(T),be_alignof(T));
+}
+
+template< typename ITERATOR >
+static ITERATOR advance(const ITERATOR& it, typename iterator_traits<ITERATOR>::difference_type offset)
+{
+    return it + offset;
+}
+
+template< typename T >
+static T* advance(T* input, difference_type offset)
+{
+    char *ptr = reinterpret_cast<char*>(input);
+    ptr = ptr + be_align(sizeof(T),be_alignof(T))*offset;
+    return reinterpret_cast<T*>(ptr);
+}
+
+template< typename T >
+static const T* advance(const T* input, difference_type offset)
+{
+    const char *ptr = reinterpret_cast<const char*>(input);
+    ptr = ptr + be_align(sizeof(T),be_alignof(T))*offset;
+    return reinterpret_cast<const T*>(ptr);
+}
+
+
+}
+
+/*****************************************************************************/
+#endif
---
+++ src/engine/core/api/minitl/container/iterator.hh
@@ -0,0 +1,97 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_MINITL_CONTAINER_INL_ITERATORS_HH_
+#define BE_MINITL_CONTAINER_INL_ITERATORS_HH_
+/*****************************************************************************/
+
+//! \addtogroup minitl
+//! @{
+namespace minitl
+{
+
+typedef size_t      size_type;
+typedef ptrdiff_t   difference_type;
+
+struct input_iterator_tag                                               { };
+struct output_iterator_tag                                              { };
+struct forward_iterator_tag : public input_iterator_tag                 { };
+struct bidirectional_iterator_tag : public forward_iterator_tag         { };
+struct random_access_iterator_tag : public bidirectional_iterator_tag   { };
+
+template< typename T, typename DIFF = ::minitl::difference_type >
+struct input_iterator
+{
+};
+
+template< typename T, typename DIFF = ::minitl::difference_type >
+struct output_iterator
+{
+};
+
+template< typename T, typename DIFF = ::minitl::difference_type >
+struct forward_iterator : public input_iterator<T,DIFF>
+{
+};
+
+template< typename T, typename DIFF = ::minitl::difference_type >
+struct bidirectional_iterator : public forward_iterator<T,DIFF>
+{
+};
+
+template< typename T, typename DIFF = ::minitl::difference_type >
+struct random_access_iterator : public bidirectional_iterator<T,DIFF>
+{
+};
+
+
+
+template< typename ITERATOR >
+struct iterator_traits
+{
+    typedef typename ITERATOR::iterator_category    iterator_category;
+    typedef typename ITERATOR::value_type           value_type;
+    typedef typename ITERATOR::pointer              pointer;
+    typedef typename ITERATOR::reference            reference;
+    typedef typename ITERATOR::const_pointer        const_pointer;
+    typedef typename ITERATOR::const_reference      const_reference;
+    typedef typename ITERATOR::size_type            size_type;
+    typedef typename ITERATOR::difference_type      difference_type;
+};
+template< typename T >
+struct iterator_traits<T*>
+{
+    typedef random_access_iterator_tag  iterator_category;
+    typedef T                           value_type;
+    typedef T*                          pointer;
+    typedef T&                          reference;
+    typedef minitl::size_type           size_type;
+    typedef minitl::difference_type     difference_type;
+};
+template< typename T >
+struct iterator_traits<const T*>
+{
+    typedef random_access_iterator_tag  iterator_category;
+    typedef const T                     value_type;
+    typedef const T*                    pointer;
+    typedef const T&                    reference;
+    typedef minitl::size_type           size_type;
+    typedef minitl::difference_type     difference_type;
+};
+
+template< typename ITERATOR >
+static typename iterator_traits<ITERATOR>::iterator_category iterator_category(const ITERATOR& it);
+
+template< typename ITERATOR >
+static typename iterator_traits<ITERATOR>::difference_type distance(const ITERATOR& first, const ITERATOR& last);
+
+template< typename ITERATOR >
+static ITERATOR advance(const ITERATOR& it, typename iterator_traits<ITERATOR>::difference_type offset);
+
+}
+//! @}
+
+#include    <minitl/container/inl/iterator.inl>
+
+/*****************************************************************************/
+#endif
