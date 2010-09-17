8a172a90f41ab7f300d5d6f3b009a9eb427d6e14
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2010-09-17 19:25:20 +0000

    missing files
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@307 f3700e90-1ba7-11de-b56b-c9554f25afe1

---
+++ src/engine/graphics/api/graphics/material/ishaderbuilder.hh
@@ -0,0 +1,25 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_GRAPHICS_MATERIAL_ISHADERBUILDER_HH_
+#define BE_GRAPHICS_MATERIAL_ISHADERBUILDER_HH_
+/*****************************************************************************/
+#include    <core/memory/streams.hh>
+
+
+namespace BugEngine { namespace Graphics
+{
+
+class be_api(GRAPHICS) IShaderBuilder
+{
+private:
+    MemoryStream<Arena::TemporaryData> m_stream;
+public:
+    IShaderBuilder();
+    ~IShaderBuilder();
+};
+
+}}
+
+/*****************************************************************************/
+#endif
---
+++ src/engine/graphics/src/material/shaders/node.cc
@@ -0,0 +1,18 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#include    <graphics/stdafx.h>
+#include    <graphics/material/shader.hh>
+
+namespace BugEngine { namespace Graphics { namespace Shaders
+{
+
+Node::Node()
+{
+}
+
+Node::~Node()
+{
+}
+
+}}}
