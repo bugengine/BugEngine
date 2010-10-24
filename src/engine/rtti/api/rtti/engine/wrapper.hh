01d115958fe93c402cdd1bce0cdd3fe2c09d41dc
mercier.nicolas@gmail.com <mercier.nicolas@gmail.com@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2010-10-24 19:47:38 +0000

    forgot one file. Removed data folder.
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@341 f3700e90-1ba7-11de-b56b-c9554f25afe1

---
+++ src/engine/rtti/api/rtti/engine/wrapper.hh
@@ -0,0 +1,27 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_RTTI_ENGINE_WRAPPER_HH_
+#define BE_RTTI_ENGINE_WRAPPER_HH_
+/*****************************************************************************/
+#include   <rtti/value.hh>
+
+namespace BugEngine { namespace RTTI
+{
+
+template< typename T, typename Owner, T (Owner::*Field) >
+static inline Value get(Value& from)
+{
+    return Value(from.as<Owner>().*Field);
+}
+
+template< typename T, typename Owner, T (Owner::*Field) >
+static inline void set(Value& object, Value& value)
+{
+    from.as<Owner>().*Field = value.as<T>();
+}
+
+}}
+
+/*****************************************************************************/
+#endif
--- src/game/discworld/data/scripts/main.lua
+++
@@ -1,4 +0,0 @@
-print("Hello, "..BugEngine.Environment:getUser().."!!")
-print("Welcome to "..BugEngine.Environment:getGame())
-print("Your home is "..BugEngine.Environment:getHomeDirectory())
-print("Holy crap, you have "..tostring(BugEngine.Environment:getProcessorCount()).." cores!!")
