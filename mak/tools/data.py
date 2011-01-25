c184b9e180d2eb7b96c269f8cadb4ccdfe015d25
mercier.nicolas@gmail.com <mercier.nicolas@gmail.com@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2011-01-25 20:38:08 +0000

    added dependency on .py files to rebuild automatically; fixed gcc build
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@432 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- mak/tools/data.py
+++ mak/tools/data.py
@@ -9,9 +9,13 @@ import sys
 
 path = os.path.abspath(os.path.split(sys.argv[0])[0])
 
+def scan(self):
+	return ([], [])
+
 def doParseData(task):
 	return mak.ddf.doParse(task.inputs[0].abspath(task.env), task.outputs[0].bldpath(task.env), path, [], ['../../mak/macros_ignore'])
 cls = Task.task_type_from_func('datagen', doParseData, [], 'GREEN', ext_in='.h .hh .hxx', ext_out='.cc', before='cc cxx')
+cls.scan = scan
 
 @extension(['.h', '.hh', '.hxx'])
 def datagen(self, node):
@@ -19,6 +23,10 @@ def datagen(self, node):
 	outs.append(node.change_ext('.cc'))
 	tsk = self.create_task('datagen', node, outs)
 	# and the c/cxx file must be compiled too
+	names = ['mak/ddf.py', 'mak/rtti.py']
+	for i in names:
+		tsk.add_file_dependency(i)
+
 	self.allnodes.append(outs[0])
 
 def detect(conf):
