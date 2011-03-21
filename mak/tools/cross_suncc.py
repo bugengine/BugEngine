29bbeff570171cc855bfbe2cf02d358bee3ee54d
mercier.nicolas@gmail.com <mercier.nicolas@gmail.com@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2011-03-21 20:23:30 +0000

    more fixes for sunCC, won't load plugins still :(
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@468 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- mak/tools/cross_suncc.py
+++ mak/tools/cross_suncc.py
@@ -22,10 +22,6 @@ def get_suncc_target(conf):
 		conf.env['SUNCC_VERSION'] = result.group(1)
 	else:
 		conf.fatal('Unable to retrieve SunCC version')
-	if conf.env['SUNCC_PLATFORM'] == 'Linux':
-		conf.env.append_unique('CCFLAGS', ['-m32', '-staticlib=Crun'])
-		conf.env.append_unique('CXXFLAGS', ['-m32', '-library=stlport4', '-staticlib=Crun,stlport4'])
-		conf.env.append_unique('LDFLAGS', ['-m32', '-library=stlport4', '-staticlib=Crun,stlport4'])
 
 @conftest
 def find_suncc(conf):
