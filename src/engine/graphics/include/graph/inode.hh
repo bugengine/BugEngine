cc360bca2bb67b408053937e8f7430f658e0057b
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2011-06-02 19:41:18 +0000

    set up the task graph and the render graph
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@537 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- src/engine/graphics/include/graph/inode.hh
+++ src/engine/graphics/include/graph/inode.hh
@@ -19,24 +19,12 @@ namespace BugEngine { namespace Graphics
 class INode : public minitl::refcountable
 {
 protected:
-    class AsyncDispatchJobGraph
-    {
-        ITask::CallbackConnection   m_renderToSync;
-        ITask::CallbackConnection   m_syncToRender;
-        ITask::CallbackConnection   m_syncToDispatch;
-        ITask::CallbackConnection   m_dispatchToSync;
-    public:
-        AsyncDispatchJobGraph(weak<ITask> renderTask, weak<ITask> syncTask, weak<ITask> dispatchTask);
-    };
-protected:
     INode();
 public:
     virtual ~INode();
 
     virtual weak<ITask> updateTask() = 0;
     virtual weak<ITask> renderTask() = 0;
-    virtual weak<ITask> syncTask() = 0;
-    virtual weak<ITask> dispatchTask() = 0;
 };
 
 }}
