2c5dfaeefdb719323c2a2e31042b17762a6a791a
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2011-07-04 17:12:58 +0000

    added package plugin
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@563 f3700e90-1ba7-11de-b56b-c9554f25afe1

---
+++ src/plugin/package/include/packageloader.hh
@@ -0,0 +1,29 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_PACKAGE_PACKAGELOADER_HH_
+#define BE_PACKAGE_PACKAGELOADER_HH_
+/*****************************************************************************/
+#include    <system/filesystem.hh>
+#include    <system/resource/resource.script.hh>
+
+namespace BugEngine { namespace Plugin { namespace PackageManager
+{
+
+class PackageLoader : public minitl::refcountable
+{
+private:
+    weak<const FileSystem> const    m_filesystem;
+public:
+    PackageLoader(weak<const FileSystem> filesystem);
+    ~PackageLoader();
+public:
+    void* operator new(size_t size, void* where)     { return ::operator new(size, where); }
+    void  operator delete(void* memory, void* where) { return ::operator delete(memory, where); }
+    void  operator delete(void* memory)              { be_notreached(); return ::operator delete(memory); }
+};
+
+}}}
+
+/*****************************************************************************/
+#endif
---
+++ src/plugin/package/src/package.cc
@@ -0,0 +1,18 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#include    <stdafx.h>
+#include    <package.script.hh>
+
+namespace BugEngine { namespace Plugin { namespace PackageManager
+{
+
+Package::Package(const ifilename& /*filename*/)
+{
+}
+
+Package::~Package()
+{
+}
+
+}}}
