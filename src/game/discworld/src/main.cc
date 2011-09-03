3674aeec425a7fe65756f945171f01572d47d072
mercier.nicolas <mercier.nicolas@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2011-09-03 19:05:43 +0000

    fixed a potential issue with the scheduler; now it always detects the endo of tasks
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@636 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- src/game/discworld/src/main.cc
+++ src/game/discworld/src/main.cc
@@ -10,7 +10,8 @@
 int be_main(weak<BugEngine::Application> app)
 {
     ref<BugEngine::FileSystem> fs = ref<BugEngine::FileSystem>::create(BugEngine::gameArena());
-    BugEngine::Plugin<minitl::pointer> plugin("GL4", weak<const BugEngine::FileSystem>(fs));
+    BugEngine::Plugin<minitl::pointer> plugin("nullrender", weak<const BugEngine::FileSystem>(fs));
     app->run();
     return 0;
 }
+
