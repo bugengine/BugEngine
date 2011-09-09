97b25783ad35780accd66e06fedb94873856971f
mercier.nicolas@gmail.com <mercier.nicolas@gmail.com@f3700e90-1ba7-11de-b56b-c9554f25afe1>
2011-09-09 10:18:00 +0000

    posix file system (copyright work from the train)
    git-svn-id: https://bugengine.googlecode.com/svn/trunk@652 f3700e90-1ba7-11de-b56b-c9554f25afe1

--- src/engine/core/api/core/platforms/platform_darwin.hh
+++ src/engine/core/api/core/platforms/platform_darwin.hh
@@ -12,6 +12,7 @@
 #include <malloc/malloc.h>
 #define malloca     alloca
 #define freea(p)
+#define DIRENT_H    <sys/dir.h>
 
 /*****************************************************************************/
 #endif
---
+++ src/engine/system/src/file/diskfolder-p=posix,darwin.cc
@@ -0,0 +1,92 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#include    <system/stdafx.h>
+#include    <system/file/diskfolder.script.hh>
+#include    <sys/types.h>
+#include    <sys/stat.h>
+#include    DIRENT_H
+#include    <errno.h>
+
+
+namespace BugEngine
+{
+
+static void createDirectory(const ipath& path, Folder::CreatePolicy policy)
+{
+    be_assert (policy != Folder::CreateNone, "invalid policy given to createDirectory");
+    if (policy == Folder::CreateRecursive)
+    {
+        ipath parent = path;
+        parent.pop_back();
+        createDirectory(parent, policy);
+    }
+    minitl::format<1024u> p = path.str();
+    if (mkdir(p.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1)
+    {
+        if (errno != EEXIST)
+        {
+            perror("");
+        }
+    }
+}
+
+DiskFolder::DiskFolder(const ipath& diskpath, Folder::ScanPolicy scanPolicy, Folder::CreatePolicy createPolicy)
+    :   m_path(diskpath)
+{
+    if(createPolicy != Folder::CreateNone) { createDirectory(diskpath, createPolicy); }
+    minitl::format<1024u> pathname = m_path.str();
+    m_handle.ptrHandle = opendir(pathname.c_str());
+    if (!m_handle.ptrHandle)
+    {
+        perror("");
+    }
+
+    if (scanPolicy != Folder::ScanNone)
+    {
+        refresh(scanPolicy);
+    }
+}
+
+DiskFolder::~DiskFolder()
+{
+    closedir((DIR*)m_handle.ptrHandle);
+}
+
+void DiskFolder::refresh(Folder::ScanPolicy scanPolicy)
+{
+    if (m_handle.ptrHandle)
+    {
+        rewinddir((DIR*)m_handle.ptrHandle);
+        while(dirent* d = readdir((DIR*)m_handle.ptrHandle))
+        {
+            if (d->d_name[0] == '.' && d->d_name[1] == 0)
+                continue;
+            if (d->d_name[0] == '.' && d->d_name[1] == '.' && d->d_name[2] == 0)
+                continue;
+            istring name = d->d_name;
+            if (d->d_type == DT_DIR)
+            {
+                for (minitl::vector< minitl::pair<istring, ref<Folder> > >::iterator it = m_folders.begin(); it != m_folders.end(); ++it)
+                {
+                    if (it->first == name)
+                    {
+                        if (scanPolicy == Folder::ScanRecursive)
+                        {
+                            it->second->refresh(scanPolicy);
+                            continue;
+                        }
+                    }
+                }
+                m_folders.push_back(minitl::make_pair(name, ref<DiskFolder>::create(fsArena(), m_path+ipath(name), scanPolicy, Folder::CreateNone)));
+            }
+            else
+            {
+                /*todo*/
+                be_info("found file: %s" | name);
+            }
+        }
+    }
+}
+
+}
