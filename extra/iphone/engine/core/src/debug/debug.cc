2c580bc2d5ceeec7ce6cee4e5f70eb942be1201d
yngwe <yngwe@HERMES.local>
2011-10-22 12:44:50 +0200

    iPhone (simulator) port

---
+++ extra/iphone/engine/core/src/debug/debug.cc
@@ -0,0 +1,43 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#include    <core/stdafx.h>
+#include    <core/debug/assert.hh>
+#include    <cstdio>
+#include    <cstdarg>
+
+namespace BugEngine { namespace Debug
+{
+
+AssertionResult defaultAssertionCallback( const char *file,
+                                          int        line,
+                                          const char *expr,
+                                          const char *message,
+                                          ...)
+{
+    fprintf(stderr, "%s:%d Assertion failed: %s\n\t", file, line, expr);
+    va_list l;
+    va_start(l, message);
+    vfprintf(stderr, message, l);
+    va_end(l);
+    fprintf(stderr, "\n");
+
+    return Ignore;
+}
+
+static AssertionCallback_t g_callback = defaultAssertionCallback;
+
+AssertionCallback_t setAssertionCallback(AssertionCallback_t callback)
+{
+    AssertionCallback_t previous = g_callback;
+    g_callback = callback;
+    return previous;
+}
+
+AssertionCallback_t getAssertionCallback()
+{
+    return g_callback;
+}
+
+}}
+
---
+++ extra/iphone/engine/core/src/threads/semaphore.cc
@@ -0,0 +1,61 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#include    <core/stdafx.h>
+#include    <core/threads/semaphore.hh>
+#include    <semaphore.h>
+#include    <cerrno>
+#include    <core/timer.hh>
+#include 	<stdio.h>
+
+namespace BugEngine
+{
+
+Semaphore::Semaphore(int initialCount, int maxCount)
+:   m_data(new sem_t)
+{
+    if (sem_init(reinterpret_cast<sem_t*>(m_data), 0, initialCount) != 0)
+    {
+        be_error("Could not initialize semaphore: %s" | strerror(errno));
+    }
+}
+
+Semaphore::~Semaphore()
+{
+    if (sem_destroy(reinterpret_cast<sem_t*>(m_data)) != 0)
+    {
+        be_error("Could not initialize semaphore: %s" | strerror(errno));
+    }
+    delete reinterpret_cast<sem_t*>(m_data);
+}
+
+void Semaphore::release(int count)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        if (sem_post(reinterpret_cast<sem_t*>(m_data)) != 0)
+        {
+            be_error("Could not release semaphore: %s" | strerror(errno));
+        }
+    }
+}
+
+Threads::Waitable::WaitResult Semaphore::wait()
+{
+    int result = sem_wait(reinterpret_cast<sem_t*>(m_data));
+    if (result == 0)
+    {
+        return Finished;
+    }
+    else
+    {
+        be_error("Could not wait on semaphore: %s" | strerror(errno));
+        be_notreached();
+        return Abandoned;
+    }
+}
+
+
+}
+
+
