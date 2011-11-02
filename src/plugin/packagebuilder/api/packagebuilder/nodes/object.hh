e05ac21553b47195f9aa1c8aed154d3232aee9c7
yngwe <yngwe@HERMES>
2011-11-02 12:19:33 +0100

    forgot files

---
+++ src/plugin/packagebuilder/api/packagebuilder/nodes/object.hh
@@ -0,0 +1,41 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_PACKAGEBUILDER_NODES_OBJECT_HH_
+#define BE_PACKAGEBUILDER_NODES_OBJECT_HH_
+/*****************************************************************************/
+
+namespace BugEngine { namespace PackageBuilder { namespace Nodes
+{
+
+class Package;
+class Parameter;
+class Reference;
+
+class Object : public minitl::refcountable
+{
+private:
+    weak< Package > const                   m_owner;
+    istring                                 m_name;
+    ref<Reference>                          m_methodReference;
+    const RTTI::MethodInfo*                 m_method;
+    const RTTI::MethodInfo::OverloadInfo*   m_overload;
+    minitl::vector< ref<Parameter> >        m_parameters;
+public:
+    Object(weak<Package> owner);
+    ~Object();
+
+    istring name() const                                    { return m_name; }
+    const RTTI::MethodInfo* method() const                  { return m_method; }
+    const RTTI::MethodInfo::OverloadInfo* overload() const  { return m_overload; }
+    const minitl::vector< ref<Parameter> >& params() const  { return m_parameters; }
+
+    void setName(istring name);
+    void setMethod(ref<Reference> reference);
+    void addParam(ref<Parameter> param);
+};
+
+}}}
+
+/*****************************************************************************/
+#endif
---
+++ src/plugin/packagebuilder/src/nodes/reference.cc
@@ -0,0 +1,30 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#include    <stdafx.h>
+#include    <packagebuilder/nodes/reference.hh>
+#include    <packagebuilder/nodes/package.hh>
+
+namespace BugEngine { namespace PackageBuilder { namespace Nodes
+{
+
+Reference::Reference(weak<Package> owner)
+    :   m_owner(owner)
+    ,   m_name("")
+    ,   m_value()
+{
+    m_owner->addReference(this);
+}
+
+Reference::~Reference()
+{
+    unhook();
+}
+
+void Reference::setName(const inamespace& name)
+{
+    m_name = name;
+    m_owner->resolveReference(this);
+}
+
+}}}
