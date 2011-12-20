cc10de580bcb418cacb631b983613e54e84c39b5
yngwe <yngwe@LABARBARA>
2011-12-20 16:23:41 +0100

    main package; ref<File>=>weak<File>, only the filesystem can own files

--- src/game/bugeditor/src/editor.cc
+++ src/game/bugeditor/src/editor.cc
@@ -4,6 +4,7 @@
 #include    <stdafx.h>
 #include    <editor.hh>
 #include    <package/package.script.hh>
+#include    <system/file/diskfolder.script.hh>
 
 namespace BugEngine { namespace Editor
 {
@@ -12,8 +13,9 @@ Editor::Editor(const PluginContext& context)
     :   m_resourceManager(scoped<ResourceManager>::create(gameArena()))
     ,   m_pluginContext(m_resourceManager)
     ,   m_packageBuilder("package", m_pluginContext)
+    ,   m_dataFolder(ref<DiskFolder>::create(gameArena(), Environment::getEnvironment().getDataDirectory()))
+    ,   m_mainPackage(ref<Package>::create(gameArena(), m_dataFolder->openFile(istring("main.pkg"))))
 {
-    
 }
 
 Editor::~Editor()
--- src/plugin/package/api/package/package.script.hh
+++ src/plugin/package/api/package/package.script.hh
@@ -1,13 +1,13 @@
 /* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
    see LICENSE for detail */
 
-#ifndef BE_RTTI_PACKAGE_SCRIPT_HH_
-#define BE_RTTI_PACKAGE_SCRIPT_HH_
+#ifndef BE_PACKAGE_PACKAGE_SCRIPT_HH_
+#define BE_PACKAGE_PACKAGE_SCRIPT_HH_
 /*****************************************************************************/
 #include    <bugengine/script.script.hh>
 #include    <rtti/tags/editor.script.hh>
 
-namespace BugEngine { namespace PackageManager
+namespace BugEngine
 {
 
 class PackageLoader;
@@ -16,11 +16,11 @@ class Package : public Script
 {
     friend class PackageLoader;
 published:
-    Package(ref<const File> file);
+    Package(weak<const File> file);
     ~Package();
 };
 
-}}
+}
 
 /*****************************************************************************/
 #endif
