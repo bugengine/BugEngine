922095fe8d3116333465058dc59d803bb62f6851
yngwe <yngwe@BENDER>
2012-04-04 09:00:03 +0200

    starting new grammar

--- mak/cpp/parser.py
+++ mak/cpp/parser.py
@@ -2,14 +2,38 @@ import sys
 try:
 	import cpp.lexer as lexer
 	import ply.yacc as yacc
+	import cpp.tokens
+	from cpp.tokens import *
 except:
 	import lexer
 	import yacc
 
+class NamespaceBegin(yacc.Nonterm):
+	"%nonterm"
+	def namespace_named(self, namespace, name, LBRACE):
+		"%reduce NAMESPACE ID LBRACE"
+		print(name.value)
+	def namespace_unnamed(self, namespace, LBRACE):
+		"%reduce NAMESPACE LBRACE"
 
+class Expr(yacc.Nonterm):
+	"%nonterm"
+	def expr_namespace(self, namespace_begin, exprs, rbrace):
+		"%reduce NamespaceBegin Exprs RBRACE"
+		
+
+class Exprs(yacc.Nonterm):
+	"%nonterm"
+	def expr_list(self, exprs, expr):
+		"%reduce Expr Exprs"
+	def empty(self):
+		"%reduce"
 
 class Header(yacc.Nonterm):
 	"%start"
+
+	def exprs(self, exprs):
+		"%reduce Exprs"
 
 
 
@@ -28,9 +52,9 @@ class Parser(yacc.Lr):
 		lexer.input(input)
 		t=lexer.token()
 		while t:
-			tok = t.token(self)
+			tok = cpp.tokens.__dict__[t.type](self)
 			tok.value = t.value
 			tok.lineno = t.lineno
-
+			self.token(tok)
 			t=lexer.token()
-
+		self.eoi()
