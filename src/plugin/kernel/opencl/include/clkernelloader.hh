4417be70882ca3a37cc18e08b9c6e1c75d4d7deb
yngwe <yngwe@LABARBARA>
2012-07-01 14:30:52 +0200

    OpenCL basic skeleton

---
+++ src/plugin/kernel/opencl/include/clkernelloader.hh
@@ -0,0 +1,27 @@
+/* BugEngine / Copyright (C) 2005-2009  screetch <screetch@gmail.com>
+   see LICENSE for detail */
+
+#ifndef BE_KERNEL_CPU_CPUKERNELLOADER_HH_
+#define BE_KERNEL_CPU_CPUKERNELLOADER_HH_
+/*****************************************************************************/
+#include    <system/scheduler/kernel/ikernelloader.hh>
+
+
+namespace BugEngine
+{
+
+class CpuKernelLoader : public IKernelLoader
+{
+public:
+    CpuKernelLoader();
+    ~CpuKernelLoader();
+
+    virtual ResourceHandle load(weak<const Resource> resource) override;
+    virtual void unload(const ResourceHandle& resource) override;
+};
+
+}
+
+
+/*****************************************************************************/
+#endif
