4feffd2013cad43c0dcfa20897f722daef8ecc09
yngwe <yngwe@ZOIDBERG>
2012-07-05 22:59:52 +0200

    Linux fixes

--- src/plugin/graphics/GL4/src/glrenderer-p=posix.cc
+++ src/plugin/graphics/GL4/src/glrenderer-p=posix.cc
@@ -192,6 +192,7 @@ GLWindow::Context::~Context()
 GLRenderer::GLRenderer(const PluginContext& context)
     :   Windowing::Renderer(Arena::general(), context.resourceManager)
     ,   m_context(scoped<Context>::create(Arena::general(), (PlatformData*)getPlatformData()))
+    ,   m_openCLScheduler("kernel.opencl.opengl", context)
 {
 }
 
