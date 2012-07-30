25cce1555d40757ddabe02d2adcc9dfa5fc3fb07
yngwe <yngwe@LABARBARA>
2012-07-30 17:50:30 +0200

    preliminary Metro port

---
+++ extra/metro/engine/core/src/threads/mutex.cc
@@ -0,0 +1,42 @@
+/* BugEngine / 2008-2012  Nicolas MERCIER <mercier.nicolas@gmail.com>
+   see LICENSE for detail */
+
+#include    <core/stdafx.h>
+#include    <core/threads/mutex.hh>
+
+
+namespace BugEngine
+{
+
+Mutex::Mutex()
+:   m_data(CreateMutex(0, FALSE, 0))
+{
+}
+
+Mutex::~Mutex()
+{
+    CloseHandle((HANDLE)m_data);
+}
+
+void Mutex::release()
+{
+    ReleaseMutex((HANDLE)m_data);
+}
+
+Threads::Waitable::WaitResult Mutex::wait()
+{
+    DWORD rcode = WaitForSingleObject((HANDLE)m_data, INFINITE);
+    switch(rcode)
+    {
+    case WAIT_OBJECT_0:
+        return Finished;
+    case WAIT_FAILED:
+        be_notreached();
+        return Abandoned;
+    case WAIT_ABANDONED:
+    default:
+        return Abandoned;
+    }
+}
+
+}
