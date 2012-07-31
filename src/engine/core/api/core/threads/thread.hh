fa8839bc22cd444cf289f47e32cf27f88a712151
yngwe <yngwe@LABARBARA>
2012-07-31 18:53:08 +0200

    more Metro fixes

--- src/engine/core/api/core/threads/thread.hh
+++ src/engine/core/api/core/threads/thread.hh
@@ -35,14 +35,11 @@ public:
     Thread(const istring& name, ThreadFunction f, intptr_t p1 = 0, intptr_t p2 = 0, Priority p = Normal);
     ~Thread();
 
-    void resume();
-
     void setPriority(Priority p);
 
     u64 id() const;
     void wait() const;
 
-    static void             sleep(int milliseconds);
     static void             yield();
     static u64              currentId();
     static istring          name();
