81450fa29e4fe8522b04787465678c018dbb8bdb
yngwe <yngwe@LABARBARA>
2012-08-25 19:29:40 +0200

    missing files

---
+++ src/engine/scheduler/src/kernel/iproduct.cc
@@ -0,0 +1,9 @@
+/* BugEngine / 2008-2012  Nicolas MERCIER <mercier.nicolas@gmail.com>
+   see LICENSE for detail */
+
+#include    <scheduler/stdafx.h>
+#include    <scheduler/kernel/iproduct.script.hh>
+
+namespace BugEngine { namespace Kernel
+{
+}}
