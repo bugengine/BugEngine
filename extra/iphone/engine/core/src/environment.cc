938ae27b272c4ed28bb7f6dab1c1638f695a0b61
yngwe <yngwe@LABARBARA>
2013-02-01 08:50:21 +0100

    removed render graph loader; cleaned up environment for iphone

--- extra/iphone/engine/core/src/environment.cc
+++ extra/iphone/engine/core/src/environment.cc
@@ -32,11 +32,9 @@ void Environment::init(int argc, const char *argv[])
     }
     *lastSlash = 0;
     filename = lastSlash + 1;
-    printf("%s\n", path);
     chdir(path);
     for( int arg = 1; arg < argc; arg++ )
     {
-        printf("%s\n", argv[arg]);
         if (argv[arg][0] == '-')
         {
             continue; // TODO
@@ -61,23 +59,7 @@ Environment& Environment::getEnvironment()
 
 size_t Environment::getProcessorCount() const
 {
-#if defined(BE_PLATFORM_LINUX) || defined(BE_PLATFORM_SUN) || defined(BE_PLATFORM_MACOS)
     return sysconf(_SC_NPROCESSORS_ONLN);
-#else
-    int numCPU = 0;
-    int mib[4];
-    size_t len = sizeof(numCPU);
-
-    mib[0] = CTL_HW;
-    mib[1] = HW_NCPU;
-    if (sysctl(mib, 2, &numCPU, &len, NULL, 0) == -1)
-    {
-        be_error("Could not retrieve number of processors: %s" | sys_errlist[errno]);
-        numCPU = 1;
-    }
-    be_assert_recover(numCPU >= 1, "Weird number of CPUs returned by sysctl: %d" | numCPU, numCPU=1);
-    return numCPU;
-#endif
 }
 
 }
