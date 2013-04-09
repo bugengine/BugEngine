f32819f5cd6b37e13c75acb41d7ca377fcc480fe
yngwe <yngwe@BENDER>
2013-04-09 17:15:03 +0200

    Win32 interrupt implementation

--- src/engine/bugengine/src/main-p=win32.cc
+++ src/engine/bugengine/src/main-p=win32.cc
@@ -3,6 +3,11 @@
 
 #include    <bugengine/stdafx.h>
 #include    <windows.h>
+#include    <iostream>
+#include    <io.h>
+#include    <stdio.h>
+#include    <fcntl.h>
+#include    <fstream>
 
 int beMain(int argc, const char *argv[]);
 
@@ -12,5 +17,29 @@ int WINAPI WinMain( HINSTANCE /*hInstance*/,
                     LPSTR /*lpCmdLine*/,
                     int /*nCmdShow*/ )
 {
+    AttachConsole(ATTACH_PARENT_PROCESS);
+    //AllocConsole();
+    if (GetConsoleWindow())
+    {
+        long stdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
+        int conHandle = _open_osfhandle(stdHandle, _O_TEXT);
+        FILE* fp = _fdopen(conHandle, "w");
+        *stdout = *fp;
+        setvbuf( stdout, NULL, _IONBF, 0 );
+
+        stdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
+        conHandle = _open_osfhandle(stdHandle, _O_TEXT);
+        fp = _fdopen( conHandle, "r" );
+        *stdin = *fp;
+        setvbuf( stdin, NULL, _IONBF, 0 );
+
+        stdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
+        conHandle = _open_osfhandle(stdHandle, _O_TEXT);
+        fp = _fdopen( conHandle, "w" );
+        *stderr = *fp;
+        setvbuf( stderr, NULL, _IONBF, 0 );
+
+        std::ios::sync_with_stdio();
+    }
     return beMain(__argc, (const char **)__argv);
 }
