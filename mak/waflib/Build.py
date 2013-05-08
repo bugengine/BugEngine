ebe705892b879ba1fd2a2a0a87ee7fbdcf9b0329
yngwe <yngwe@LABARBARA>
2013-05-08 07:42:53 +0200

    do not print files that are already installed

--- mak/waflib/Build.py
+++ mak/waflib/Build.py
@@ -952,8 +952,6 @@ class InstallContext(BuildContext):
 			else:
 				# same size and identical timestamps -> make no copy
 				if st1.st_mtime + 2 >= st2.st_mtime and st1.st_size == st2.st_size:
-					if not self.progress_bar:
-						Logs.info('- install %s (from %s)' % (tgt, srclbl))
 					return False
 
 		if not self.progress_bar:
