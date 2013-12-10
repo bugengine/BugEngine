fe706c188574414c7ff8d4e02fa7f4ddc6787a7b
yngwe <yngwe@LABARBARA>
2013-12-10 08:36:08 +0100

    automated boilerplate generation of kernel

--- src/sample/kernel/kernels/add.cc
+++ src/sample/kernel/kernels/add.cc
@@ -1,17 +1,6 @@
 /* BugEngine / 2008-2012  Nicolas MERCIER <mercier.nicolas@gmail.com>
    see LICENSE for detail */
 
-/* BEGIN BOILERPLATE */
-#define be_api(X)
-#include    <kernel/compilers.hh>
-#include    <kernel/simd.hh>
-#include    <kernel/input.hh>
-#include    <plugin/dynobjectlist.hh>
-using namespace Kernel;
-#define be_tag(x)
-#define be_product(x)
-/* END BOILERPLATE */
-
 #include <components.script.hh>
 
 void kmain(u32 index, const u32 total,
@@ -35,24 +24,3 @@ void kmain(u32 index, const u32 total,
         ++first;
     }
 }
-
-
-/* BEGIN BOILERPLATE */
-struct Parameter
-{
-    void* begin;
-    void* end;
-};
-
-_BE_PLUGIN_EXPORT void _kmain(const u32 index, const u32 total, Parameter argv[])
-{
-    kmain(index, total,
-          in<BugEngine::A>((BugEngine::A*)argv[0].begin, (BugEngine::A*)argv[0].end),
-          inout<BugEngine::B>((BugEngine::B*)argv[1].begin, (BugEngine::B*)argv[1].end),
-          inout<BugEngine::C>((BugEngine::C*)argv[2].begin, (BugEngine::C*)argv[2].end),
-          inout<BugEngine::D>((BugEngine::D*)argv[3].begin, (BugEngine::D*)argv[3].end));
-}
-_BE_REGISTER_PLUGIN(BE_KERNELID, BE_KERNELNAME);
-_BE_REGISTER_METHOD(BE_KERNELID, _kmain);
-
-/* END BOILERPLATE */
