e69d83420bd4608d9aae910b1ec973c3e449e31c
yngwe <yngwe@LABARBARA>
2014-01-02 18:08:50 +0100

    component back link to entity; bug fixes

--- src/sample/kernel/include/components.script.hh
+++ src/sample/kernel/include/components.script.hh
@@ -23,70 +23,40 @@ be_pod A : public World::Component
 {
     u32 value;
 
-    void created()
-    {
-        be_info("created A(%d)" | value);
-    }
-    void destroyed()
-    {
-        be_info("destroyed A(%d)" | value);
-    }
+    void created();
+    void destroyed();
 };
 
 be_pod B : public World::Component
 {
     u32 value;
 
-    void created()
-    {
-        be_info("created B(%d)" | value);
-    }
-    void destroyed()
-    {
-        be_info("destroyed B(%d)" | value);
-    }
+    void created();
+    void destroyed();
 };
 
 be_pod C : public World::Component
 {
     u32 value;
 
-    void created()
-    {
-        be_info("created C(%d)" | value);
-    }
-    void destroyed()
-    {
-        be_info("destroyed C(%d)" | value);
-    }
+    void created();
+    void destroyed();
 };
 
 be_pod D : public World::Component
 {
     u32 value;
 
-    void created()
-    {
-        be_info("created D(%d)" | value);
-    }
-    void destroyed()
-    {
-        be_info("destroyed D(%d)" | value);
-    }
+    void created();
+    void destroyed();
 };
 
 be_pod E : public World::Component
 {
     u32 value;
 
-    void created()
-    {
-        be_info("created E(%d)" | value);
-    }
-    void destroyed()
-    {
-        be_info("destroyed E(%d)" | value);
-    }
+    void created();
+    void destroyed();
 };
 
 
