90fbc29845494eac9ac618fee0a4dcf6d75ff963
yngwe <yngwe@BENDER>
2014-02-18 17:07:34 +0100

    added font loading library skeleton

---
+++ src/plugin/graphics/text/include/fontlist.hh
@@ -0,0 +1,22 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#ifndef BE_TEXT_FONTLIST_HH_
+#define BE_TEXT_FONTLIST_HH_
+/*****************************************************************************/
+#include    <minitl/pointer.hh>
+
+namespace BugEngine
+{
+
+class FontList : public minitl::pointer
+{
+public:
+    FontList();
+    ~FontList();
+};
+
+}
+
+/*****************************************************************************/
+#endif
---
+++ src/plugin/graphics/text/src/text/text.cc
@@ -0,0 +1,19 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <text/stdafx.h>
+#include    <text/text.script.hh>
+
+namespace BugEngine
+{
+
+Text::Text()
+{
+}
+
+Text::~Text()
+{
+}
+
+}
+
