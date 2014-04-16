73e60cd5a9aa110ff72a61e5d8619a3dae12becd
yngwe <yngwe@LABARBARA>
2014-04-16 08:44:57 +0200

    entity operation

---
+++ src/engine/world/include/componentbucket.hh
@@ -0,0 +1,27 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#ifndef BE_WORLD_COMPONENTBUCKET_HH_
+#define BE_WORLD_COMPONENTBUCKET_HH_
+/**************************************************************************************************/
+#include    <world/stdafx.h>
+
+namespace BugEngine { namespace World
+{
+
+struct Bucket
+{
+    u32* componentCounts;
+    u32 acceptMask;
+    u32 maskSize;
+    u32 firstComponent;
+
+    Bucket();
+    Bucket(u32* componentCounts, u32 acceptMask);
+};
+
+}}
+
+
+/**************************************************************************************************/
+#endif
