6210dbe129b03c8e00241b554bbbb842be93a48d
yngwe <yngwe@LABARBARA>
2014-05-25 16:35:44 +0200

    MacOS X universal fixes; python, OpenGL, OpenCL

--- src/plugin/scripting/pythonbinding/src/context.cc
+++ src/plugin/scripting/pythonbinding/src/context.cc
@@ -72,14 +72,16 @@ void Context::unload(Resource::Resource& /*handle*/)
     PyEval_ReleaseThread(m_pythonState);
 }
 
-void Context::runBuffer(weak<const PythonScript> /*script*/, Resource::Resource& /*resource*/, const minitl::Allocator::Block<u8>& block)
+void Context::runBuffer(weak<const PythonScript> /*script*/, Resource::Resource& /*resource*/,
+                        const minitl::Allocator::Block<u8>& block)
 {
     PyEval_AcquireThread(m_pythonState);
     PyRun_SimpleString((const char*)block.begin());
     PyEval_ReleaseThread(m_pythonState);
 }
 
-void Context::reloadBuffer(weak<const PythonScript> /*script*/, Resource::Resource& /*resource*/, const minitl::Allocator::Block<u8>& block)
+void Context::reloadBuffer(weak<const PythonScript> /*script*/, Resource::Resource& /*resource*/,
+                           const minitl::Allocator::Block<u8>& block)
 {
     PyEval_AcquireThread(m_pythonState);
     PyRun_SimpleString((const char*)block.begin());
