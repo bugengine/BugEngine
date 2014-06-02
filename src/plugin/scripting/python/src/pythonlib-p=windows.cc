a6ac3cf5ba331bb2fa2b03b869e1fe7ee7b568a1
yngwe <yngwe@BENDER>
2014-06-02 17:11:39 +0200

    python fix for windows

---
+++ src/plugin/scripting/python/src/pythonlib-p=windows.cc
@@ -0,0 +1,68 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+see LICENSE for detail */
+
+#include    <python/stdafx.h>
+#include    <python/pythonlib.hh>
+
+namespace BugEngine { namespace Python
+{
+
+PythonLibrary::PythonLibrary(const char* pythonLibraryName)
+    : m_pythonLibraryName(pythonLibraryName)
+    , m_handle(m_pythonLibraryName
+                    ? LoadLibraryA(minitl::format<1024u>("%s.dll") | m_pythonLibraryName)
+                    : GetModuleHandle(NULL))
+    , m_status(m_handle != 0)
+{
+    if (!m_status)
+    {
+        be_error("unable to load library %s" | pythonLibraryName);
+    }
+    else
+    {
+#   define be_get_func(f)                                                   \
+        void* tmp##f = GetProcAddress((HMODULE)m_handle, #f);               \
+        memcpy(&m_##f, &tmp##f, sizeof(f##Func));                           \
+        if (!m_##f)                                                         \
+        {                                                                   \
+            be_error("could not locate function %s in module %s"            \
+                        | #f                                                \
+                        | (pythonLibraryName ? pythonLibraryName : "root"));\
+            m_status = false;                                               \
+        }
+        be_get_func(Py_SetPythonHome);
+        be_get_func(Py_InitializeEx);
+        be_get_func(Py_Finalize);
+        be_get_func(Py_NewInterpreter);
+        be_get_func(Py_EndInterpreter);
+        be_get_func(PyEval_InitThreads);
+        be_get_func(PyEval_SaveThread);
+        be_get_func(PyEval_AcquireThread);
+        be_get_func(PyEval_ReleaseThread);
+        be_get_func(PyEval_ReleaseLock);
+        be_get_func(PyRun_SimpleString);
+#   undef be_get_fun
+        if (m_status && pythonLibraryName)
+        {
+            initialize();
+        }
+    }
+}
+
+PythonLibrary::~PythonLibrary()
+{
+    if (m_status && m_pythonLibraryName)
+    {
+        finalize();
+    }
+    if (m_handle && m_pythonLibraryName)
+    {
+        FreeLibrary((HMODULE)m_handle);
+    }
+}
+
+void PythonLibrary::platformInitialize()
+{
+}
+
+}}
