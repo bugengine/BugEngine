9f8bae61edeaab913cf4b320065f136cb0de453a
yngwe <yngwe@LABARBARA>
2014-08-19 08:52:27 +0200

    Doc generation step

--- mak/tools/data.py
+++ mak/tools/data.py
@@ -16,7 +16,37 @@ cls = Task.task_factory('datagen', ddf, [], 'PINK', ext_in='.h .hh .hxx', ext_ou
 cls.scan = scan
 
 class docgen(Task.Task):
+    def process_node_gcode(self, node):
+        return []
+
+    def process_node_github(self, node):
+        return []
+    
+    def cleanup_gcode(self):
+        files = self.out_dir_gcode.ant_glob()
+        for file in files:
+            if file.name.startswith('api.'):
+                if file not in self.outputs:
+                    file.delete()
+
+    def cleanup_github(self):
+        files = self.out_dir_github.ant_glob()
+        for file in files:
+            if file not in self.outputs:
+                while file:
+                    file.delete()
+                    file = file.parent
+                    if file.children:
+                        break
+                    if file == self.out_dir:
+                        break
+        
     def run(self):
+        for input in self.inputs:
+            self.outputs += self.process_node_gcode(input)
+            self.outputs += self.process_node_github(input)
+        self.cleanup_gcode()
+        self.cleanup_github()
         return 0
 
 @extension('.h', '.hh', '.hxx')
@@ -62,4 +92,8 @@ def docgen(self, node):
         doc_task = self.bld.doc_task
     except AttributeError:
         doc_task = self.bld.doc_task = self.create_task('docgen', [], [])
+        doc_task.out_dir_gcode = self.bld.srcnode.make_node('doc/wiki.googlecode')
+        doc_task.out_dir_gcode.mkdir()
+        doc_task.out_dir_github = self.bld.srcnode.make_node('doc/wiki.github/api')
+        doc_task.out_dir_github.mkdir()
     doc_task.set_inputs([node])
