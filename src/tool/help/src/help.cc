f1beaa54a3e6228129f7cdbf00bad3f1e3ea2094
yngwe <yngwe@LABARBARA>
2014-08-20 08:45:39 +0200

    added Help tool

---
+++ src/tool/help/src/help.cc
@@ -0,0 +1,22 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <stdafx.h>
+#include    <help.hh>
+#include    <filesystem/diskfolder.script.hh>
+
+
+namespace BugEngine
+{
+
+Help::Help(const Plugin::Context& context)
+    :   Application(ref<DiskFolder>::create(Arena::game(), Environment::getEnvironment().getDataDirectory()), context.scheduler)
+{
+    be_info("Help viewer");
+}
+
+Help::~Help()
+{
+}
+
+}
