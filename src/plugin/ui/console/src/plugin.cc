54dd12ff24bd1c9c0355bbcbf6edd531ee38ec0c
yngwe <yngwe@LABARBARA>
2014-08-21 09:08:37 +0200

    console UI intermediate plugin stub

---
+++ src/plugin/ui/console/src/plugin.cc
@@ -0,0 +1,9 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <console/stdafx.h>
+#include    <plugin/plugin.hh>
+#include    <rtti/engine/namespace.hh>
+#include    <console/uiconsole.hh>
+
+BE_PLUGIN_REGISTER(BugEngine::UIConsole);
---
+++ src/plugin/ui/console/src/uiconsole.cc
@@ -0,0 +1,18 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <console/stdafx.h>
+#include    <console/uiconsole.hh>
+
+namespace BugEngine
+{
+
+UIConsole::UIConsole(const Plugin::Context& context)
+{
+}
+
+UIConsole::~UIConsole()
+{
+}
+
+}
