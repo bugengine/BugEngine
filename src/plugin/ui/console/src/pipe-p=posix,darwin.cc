2889633d4c744aedb6ac2c12c95a5c31123c8f91
yngwe <yngwe@LABARBARA>
2014-09-04 18:17:31 +0200

    fixes to ncurses handling

---
+++ src/plugin/ui/console/src/pipe-p=posix,darwin.cc
@@ -0,0 +1,28 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <console/stdafx.h>
+#include    <window.script.hh>
+#include    <unistd.h>
+#include    <errno.h>
+#include    <stdio.h>
+
+namespace BugEngine
+{
+
+Window::ScreenPipe::ScreenPipe()
+{
+    int pipeDescriptors[2];
+    int result = pipe(pipeDescriptors);
+    be_assert(result == 0, "CreatePipe failed with error %s" | strerror(errno));
+    m_screenStdOut = fdopen(pipeDescriptors[0], "rt");
+    m_fileOutput = fdopen(pipeDescriptors[1], "wt");
+}
+
+Window::ScreenPipe::~ScreenPipe()
+{
+    fclose(m_fileOutput);
+    fclose(m_screenStdOut);
+}
+
+}
