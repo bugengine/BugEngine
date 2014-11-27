9a3197ce56d7db6c3e1278c40e26f918b8c071c6
yngwe <yngwe@LABARBARA>
2014-11-27 08:49:10 +0100

    entity storage minor progress

--- src/sample/kernel/src/storage.cc
+++ src/sample/kernel/src/storage.cc
@@ -8,47 +8,47 @@
 namespace BugEngine
 {
 
-void A::created()
+void A::created(World::World& /*world*/)
 {
     be_info("created A (%d)" | value);
 }
-void A::destroyed()
+void A::destroyed(World::World& /*world*/)
 {
     be_info("destroyed A (%d)" | value);
 }
 
-void B::created()
+void B::created(World::World& /*world*/)
 {
     be_info("created B (%d)" | value);
 }
-void B::destroyed()
+void B::destroyed(World::World& /*world*/)
 {
     be_info("destroyed B (%d)" | value);
 }
 
-void C::created()
+void C::created(World::World& /*world*/)
 {
     be_info("created C (%d)" | value);
 }
-void C::destroyed()
+void C::destroyed(World::World& /*world*/)
 {
     be_info("destroyed C (%d)" | value);
 }
 
-void D::created()
+void D::created(World::World& /*world*/)
 {
     be_info("created D (%d)" | value);
 }
-void D::destroyed()
+void D::destroyed(World::World& /*world*/)
 {
     be_info("destroyed D (%d)" | value);
 }
 
-void E::created()
+void E::created(World::World& /*world*/)
 {
     be_info("created E (%d)" | value);
 }
-void E::destroyed()
+void E::destroyed(World::World& /*world*/)
 {
     be_info("destroyed E (%d)" | value);
 }
