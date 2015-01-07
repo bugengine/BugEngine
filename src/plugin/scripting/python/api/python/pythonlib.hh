556668aef65b73918518d977a760b53e7dfce8fc
yngwe <yngwe@BENDER>
2015-01-07 10:50:26 +0100

    function type fix

--- src/plugin/scripting/python/api/python/pythonlib.hh
+++ src/plugin/scripting/python/api/python/pythonlib.hh
@@ -51,7 +51,7 @@ private:
     int                                     m_api;
     int                                     m_version;
     PyThreadState*                          m_mainThread;
-private: // friend ThreadLock               
+private: // friend ThreadLock
     Py_SetPythonHomeType                    m_Py_SetPythonHome;
     Py_InitializeExType                     m_Py_InitializeEx;
     Py_FinalizeType                         m_Py_Finalize;
@@ -59,20 +59,20 @@ private: // friend ThreadLock
     Py_EndInterpreterType                   m_Py_EndInterpreter;
     Py_GetPathType                          m_Py_GetPath;
     Py_GetVersionType                       m_Py_GetVersion;
-                                            
+
     PyEval_InitThreadsType                  m_PyEval_InitThreads;
     PyEval_SaveThreadType                   m_PyEval_SaveThread;
     PyEval_AcquireThreadType                m_PyEval_AcquireThread;
     PyEval_ReleaseThreadType                m_PyEval_ReleaseThread;
     PyEval_ReleaseLockType                  m_PyEval_ReleaseLock;
-public:                                     
+public:
     PyRun_SimpleStringType                  m_PyRun_SimpleString;
     Py_InitModule4Type                      m_Py_InitModule4;
     Py_InitModule4_64Type                   m_Py_InitModule4_64;
     PyModule_Create2Type                    m_PyModule_Create2;
     PyModule_AddObjectType                  m_PyModule_AddObject;
     _Py_NoneStructType                      m__Py_NoneStruct;
-                                            
+
     PyObject_SetAttrStringType              m_PyObject_SetAttrString;
     PyObject_GetAttrStringType              m_PyObject_GetAttrString;
     _PyArg_ParseTuple_SizeTType             m__PyArg_ParseTuple_SizeT;
@@ -87,21 +87,21 @@ public:
     PyList_AppendType                       m_PyList_Append;
     PyList_GetSliceType                     m_PyList_GetSlice;
     PyList_SetSliceType                     m_PyList_SetSlice;
-                                            
+
     PyString_FromStringType                 m_PyString_FromString;
     PyString_FromStringAndSizeType          m_PyString_FromStringAndSize;
     PyString_FromFormatType                 m_PyString_FromFormat;
     PyString_SizeType                       m_PyString_Size;
     PyString_AsStringType                   m_PyString_AsString;
     PyString_AsStringAndSizeType            m_PyString_AsStringAndSize;
-                                            
+
     PyUnicode_FromStringType                m_PyUnicode_FromString;
     PyUnicode_FromStringAndSizeType         m_PyUnicode_FromStringAndSize;
     PyUnicode_FromFormatType                m_PyUnicode_FromFormat;
 
     PyBool_FromLongType                     m_PyBool_FromLong;
     PyInt_FromLongType                      m_PyInt_FromLong;
-    PyInt_FromLongType                      m_PyFloat_FromDouble;
+    PyFloat_FromDoubleType                  m_PyFloat_FromDouble;
 };
 
 extern ref<PythonLibrary> s_library;
