d3687b1b57f666143dbd1ef5d05f350afe34955a
yngwe@LABARBARA <bugengine.devel@gmail.com>
2015-10-10 16:58:07 +0200

    Further simplified task splitting and queueing

--- src/engine/scheduler/api/scheduler/private/taskitem.hh
+++ src/engine/scheduler/api/scheduler/private/taskitem.hh
@@ -21,18 +21,9 @@ protected:
     weak<ITask> m_owner;
 protected:
     template< typename T >
-    void* allocate(weak<Scheduler> scheduler)
-    {
-        return scheduler->allocateTask<T>();
-    }
-    template< typename T >
     void release(weak<Scheduler> scheduler)
     {
         return scheduler->releaseTask<T>(minitl::be_checked_cast<T>(this));
-    }
-    void schedule(weak<Scheduler> scheduler, ITaskItem* newItem)
-    {
-        return scheduler->queueTask(newItem, Scheduler::Immediate);
     }
 public:
     virtual void    run(weak<Scheduler> sc) = 0;
@@ -45,15 +36,14 @@ public:
 template< typename Range, typename Body >
 class TaskItem : public ITaskItem
 {
+    friend class Task<Body>;
 private:
     Range       m_range;
     const Body& m_body;
 public:
-    TaskItem(weak< Task<Body> > owner, const Range& r, const Body& b);
-
     virtual void    run(weak<Scheduler> sc) override;
 private:
-    TaskItem(const TaskItem& split, u32 index, u32 total);
+    TaskItem(weak< Task<Body> > owner, const Range& r, u32 index, u32 total);
 private:
     TaskItem(const TaskItem& other);
     TaskItem& operator=(const TaskItem& other);
