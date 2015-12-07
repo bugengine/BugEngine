027ec6f98254d7d26eb779dd3b9e704d45a4c406
yngwe@LABARBARA <bugengine.devel@gmail.com>
2015-12-07 09:31:44 +0100

    Python improvement: use filename in output for exception/error report

--- src/plugin/scripting/python/src/context.cc
+++ src/plugin/scripting/python/src/context.cc
@@ -24,18 +24,77 @@ void Context::unload(Resource::Resource& /*handle*/)
     PythonLibrary::ThreadLock lock(m_library, m_pythonState);
 }
 
-void Context::runBuffer(weak<const PythonScript> /*script*/, Resource::Resource& /*resource*/,
+void Context::runBuffer(weak<const PythonScript> script, Resource::Resource& /*resource*/,
                         const minitl::Allocator::Block<u8>& block)
 {
     PythonLibrary::ThreadLock lock(m_library, m_pythonState);
-    (*m_library->m_PyRun_SimpleString)((const char*)block.begin());
+    PyCodeObject* code = 0;
+    if (m_library->m_Py_CompileStringExFlags)
+    {
+        code = (*m_library->m_Py_CompileStringExFlags)((const char*)block.begin(),
+                                                       script->getScriptName().str().name,
+                                                       Py_file_input, NULL,
+                                                       -1);
+    }
+    else
+    {
+        code = (*m_library->m_Py_CompileStringFlags)((const char*)block.begin(),
+                                                     script->getScriptName().str().name,
+                                                     Py_file_input, NULL);
+    }
+    if (code)
+    {
+        PyObject* m = (*m_library->m_PyImport_AddModule)("__main__");
+        PyObject* d = (*m_library->m_PyModule_GetDict)(m);
+        PyObject* result = (*m_library->m_PyEval_EvalCodeEx)(code, d, d, NULL, 0,
+                                                             NULL, 0, NULL, 0, NULL);
+        if (!result)
+        {
+            (*m_library->m_PyErr_Print)();
+        }
+        else
+        {
+            Py_DECREF(result);
+        }
+        Py_DECREF(code);
+    }
 }
 
-void Context::reloadBuffer(weak<const PythonScript> /*script*/, Resource::Resource& /*resource*/,
+void Context::reloadBuffer(weak<const PythonScript> script, Resource::Resource& /*resource*/,
                            const minitl::Allocator::Block<u8>& block)
 {
     PythonLibrary::ThreadLock lock(m_library, m_pythonState);
-    (*m_library->m_PyRun_SimpleString)((const char*)block.begin());
+    PyCodeObject* code = 0;
+    if (m_library->m_Py_CompileStringExFlags)
+    {
+        code = (*m_library->m_Py_CompileStringExFlags)((const char*)block.begin(),
+                                                       script->getScriptName().str().name,
+                                                       Py_file_input, NULL,
+                                                       -1);
+    }
+    else
+    {
+        code = (*m_library->m_Py_CompileStringFlags)((const char*)block.begin(),
+                                                     script->getScriptName().str().name,
+                                                     Py_file_input, NULL);
+    }
+    if (code)
+    {
+        PyObject* m = (*m_library->m_PyImport_AddModule)("__main__");
+        PyObject* d = (*m_library->m_PyModule_GetDict)(m);
+        PyObject* result = (*m_library->m_PyEval_EvalCodeEx)(code, d, d, NULL, 0,
+                                                             NULL, 0, NULL, 0, NULL);
+        if (!result)
+        {
+            (*m_library->m_PyErr_Print)();
+        }
+        else
+        {
+            Py_DECREF(result);
+        }
+        Py_DECREF(code);
+    }
+    Py_DECREF(code);
 }
 
 }}
