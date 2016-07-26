34d4fcec70852f928b4d5f1e356c945aefed6661
yngwe@ZOIDBERG <yngwe@ZOIDBERG>
2016-07-26 18:09:39 +0200

    fixed 3rdparty checks that were too lenient

--- src/3rdparty/OpenAL/OpenAL.py
+++ src/3rdparty/OpenAL/OpenAL.py
@@ -7,4 +7,8 @@ def setup(conf):
     if 'darwin' in conf.env.VALID_PLATFORMS:
         conf.check_framework('OpenAL', var='ALFramework')
     else:
-        conf.check_lib('openal', var='OpenAL', libpath=[os.path.join(conf.path.abspath(), 'lib.%s.%s'%(conf.env.VALID_PLATFORMS[0], a)) for a in conf.env.VALID_ARCHITECTURES])
+        conf.check_lib('openal', var='OpenAL',
+                    libpath=[os.path.join(conf.path.abspath(), 'lib.%s.%s'%(conf.env.VALID_PLATFORMS[0], a)) for a in conf.env.VALID_ARCHITECTURES],
+                    includepath=[os.path.join(conf.path.abspath(), 'api')],
+                    includes=['AL/alc.h', 'AL/al.h'],
+                    functions=['alcOpenDevice(0)'])
