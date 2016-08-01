be3069693dbd374ed9096389913fff18bc23d2af
yngwe@labarbara <bugengine.devel@gmail.com>
2016-08-01 15:55:31 +0200

    win32 build fix

--- src/3rdparty/dbghelp/dbghelp.py
+++ src/3rdparty/dbghelp/dbghelp.py
@@ -7,5 +7,5 @@ def setup(conf):
     conf.check_lib(['psapi', 'dbghelp', 'version'], var='PSAPI',
                    libpath=[os.path.join(conf.path.abspath(),
                    'lib.%s.%s'%(conf.env.VALID_PLATFORMS[0], a)) for a in conf.env.VALID_ARCHITECTURES],
-                   includes=['dbghelp.h'],
+                   includes=['windows.h', 'dbghelp.h'],
                    functions=['SymInitialize(0, 0, 0)'])
