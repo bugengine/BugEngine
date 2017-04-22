a2134ed3099eb781e76b34dd635ae4a1bf21ec6b
yngwe <yngwe@LABARBARA>
2017-04-22 22:26:19 +0200

    fixed 3rdparty lib calls

--- src/3rdparty/OpenGLES2/OpenGLES2.py
+++ src/3rdparty/OpenGLES2/OpenGLES2.py
@@ -12,10 +12,10 @@ def setup(conf):
                        'lib.%s.%s'%(conf.env.VALID_PLATFORMS[0], a)) for a in conf.env.VALID_ARCHITECTURES],
                        includepath=[os.path.join(conf.path.abspath(), 'api')],
                        includes=['GLES2/gl2.h'],
-                       functions=['glGenFramebuffers(0, 0)'])
+                       functions=['glGenFramebuffers'])
         conf.check_lib('EGL',
                        libpath=[os.path.join(conf.path.abspath(),
                        'lib.%s.%s'%(conf.env.VALID_PLATFORMS[0], a)) for a in conf.env.VALID_ARCHITECTURES],
                        includepath=[os.path.join(conf.path.abspath(), 'api')],
                        includes=['EGL/egl.h'],
-                       functions=['eglCreateContext(0, 0, 0, 0)'])
+                       functions=['eglCreateContext'])
