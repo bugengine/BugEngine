40fb96e1e4e8081984a44edcd9ab7776391c4059
yngwe@FRY <yngwe@FRY>
2017-06-12 00:12:34 +0200

    added Lua sample, similar to Python sample

---
+++ src/sample/lua/src/luasample.cc
@@ -0,0 +1,26 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <stdafx.h>
+#include    <luasample.hh>
+#include    <core/environment.hh>
+#include    <filesystem/diskfolder.script.hh>
+
+namespace BugEngine
+{
+
+LuaSample::LuaSample(const Plugin::Context& context)
+    :   Application(ref<DiskFolder>::create(Arena::game(), Environment::getEnvironment().getDataDirectory()), context.scheduler)
+    ,   m_packageManager("plugin.scripting.package", pluginContext())
+    ,   m_luaManager("plugin.scripting.lua", pluginContext())
+    ,   m_mainPackage(ref<Package>::create(Arena::game(), pluginContext().dataFolder->openFile(istring("sample-lua.pkg"))))
+{
+    pluginContext().resourceManager->load(m_mainPackage);
+}
+
+LuaSample::~LuaSample()
+{
+    pluginContext().resourceManager->unload(m_mainPackage);
+}
+
+}
