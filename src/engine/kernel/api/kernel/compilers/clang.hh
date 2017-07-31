580143719e4908d070637c11729829f370bcf612
yngwe@FRY <yngwe@FRY>
2017-07-31 23:50:00 +0200

    fixes to QtCReator project generation; fixed breakpoint instruction for clang

--- src/engine/kernel/api/kernel/compilers/clang.hh
+++ src/engine/kernel/api/kernel/compilers/clang.hh
@@ -13,7 +13,7 @@
 #elif defined(_MIPS)
 # define be_break()             __asm("break")
 #elif defined(_ARM64)
-# define be_break()             __asm__ volatile(".inst 0xd4200000")
+# define be_break()             __asm__ volatile("brk 0x0")
 #elif defined(_ARM) && !defined(__thumb__)
 # define be_break()             __asm__ volatile(".inst 0xe7f001f0")
 #elif defined(_ARM)
