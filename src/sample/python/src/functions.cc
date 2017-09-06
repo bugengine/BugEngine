5aeeba63693a69aa0eff338ed5dc427d3aea9b88
yngwe@FRY <yngwe@FRY>
2017-09-06 15:03:54 +0200

    Python sample extras

---
+++ src/sample/python/src/functions.cc
@@ -0,0 +1,12 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#include    <stdafx.h>
+#include    <rtti/engine/namespace.hh>
+
+BE_REGISTER_NAMESPACE_2(BugEngine, TestCases)
+
+namespace BugEngine
+{
+
+}
