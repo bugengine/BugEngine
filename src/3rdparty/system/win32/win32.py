970729cf725b3f62dc0c7147c3241529fa972d83
yngwe@FRY <yngwe@FRY>
2017-10-05 13:38:26 +0200

    Fixed MSVC builds
    Restore include path as it used to be, so MSVC can find the dummy unistd.h header.

--- src/3rdparty/system/win32/win32.py
+++ src/3rdparty/system/win32/win32.py
@@ -3,4 +3,5 @@ def options(opt):
 
 def setup(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
-        conf.check_lib(['kernel32', 'user32', 'advapi32', 'ole32', 'oleaut32', 'uuid', 'shell32'])
+        conf.check_lib(['kernel32', 'user32', 'advapi32', 'ole32', 'oleaut32', 'uuid', 'shell32'],
+                       includepath=[os.path.join(conf.path.abspath(), 'api.windows'))
