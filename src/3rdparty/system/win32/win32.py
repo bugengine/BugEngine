45ff344f4e0671fae305b0f3847dbf20c33aa22b
yngwe@FRY <yngwe@FRY>
2017-10-05 13:59:51 +0200

    Fixed syntax error

--- src/3rdparty/system/win32/win32.py
+++ src/3rdparty/system/win32/win32.py
@@ -4,4 +4,4 @@ def options(opt):
 def setup(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
         conf.check_lib(['kernel32', 'user32', 'advapi32', 'ole32', 'oleaut32', 'uuid', 'shell32'],
-                       includepath=[os.path.join(conf.path.abspath(), 'api.windows'))
+                       includepath=[os.path.join(conf.path.abspath(), 'api.windows')])
