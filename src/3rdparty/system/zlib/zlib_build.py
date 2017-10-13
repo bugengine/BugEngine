e44bfa266ead3ead6972d881a236c4527fccb7f3
yngwe <yngwe@leela>
2017-10-13 11:20:22 +0200

    Zlib honors static builds
    Undef ZLIB_DLL when building statics. When it was defined, it caused
    link errors when linking statically.

--- src/3rdparty/system/zlib/zlib_build.py
+++ src/3rdparty/system/zlib/zlib_build.py
@@ -3,10 +3,11 @@ from waflib import Options
 
 def build(bld):
     if not bld.env.SYSTEM_ZLIB or Options.options.included_zlib or bld.env.PROJECTS:
+        dll_defs = (not bld.env.STATIC) and ['ZLIB_DLL'] or []
         bld.shared_library('3rdparty.system.zlib', bld.platforms,
                            path='.', use_master=False,
-                           extra_defines=['ZLIB_DLL', 'ZLIB_INTERNAL'],
-                           extra_public_defines=['ZLIB_DLL'],
+                           extra_defines=dll_defs + ['ZLIB_INTERNAL'],
+                           extra_public_defines=dll_defs,
                            warnings=False, export_all=True)
     else:
         bld.thirdparty('3rdparty.system.zlib')
