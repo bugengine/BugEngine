cc64cd0f15d1ea3c5022e317a728fa5f6433f401
yngwe <yngwe@leela>
2017-10-14 22:10:34 +0200

    Made namespaces privates as they do not need to be exported

--- src/engine/rtti/api/rtti/engine/namespace.hh
+++ src/engine/rtti/api/rtti/engine/namespace.hh
@@ -13,7 +13,8 @@
 #define BE_REGISTER_NAMESPACE_1_NAMED(plugin, n)                                                                                                \
 namespace BugEngine                                                                                                                             \
 {                                                                                                                                               \
-    BE_EXPORT raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n()                                                                         \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace();                                                                                      \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n()                                                                                   \
     {                                                                                                                                           \
         static RTTI::Class ci = { #n, 0, 0, RTTI::ClassType_Namespace, {0}, {0}, {0}, {0}, {0, 0}, {0, 0}, {0}, {0}, 0, 0 };                    \
         raw<BugEngine::RTTI::Class> ptr = {&ci};                                                                                                \
@@ -27,7 +28,8 @@ namespace BugEngine                                                             
 #define BE_REGISTER_NAMESPACE_2_NAMED(plugin, n1, n2)                                                                                           \
 namespace BugEngine                                                                                                                             \
 {                                                                                                                                               \
-    BE_EXPORT raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2()                                                                 \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1();                                                                                 \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2()                                                                           \
     {                                                                                                                                           \
         static RTTI::Class ci = { #n2, 0, 0, RTTI::ClassType_Namespace, {be_##plugin##_Namespace_##n1().m_ptr}, {0}, {0},                       \
                                   {0}, {0, 0}, {0, 0}, {0}, {0}, 0, 0 };                                                                        \
@@ -43,7 +45,8 @@ namespace BugEngine                                                             
 #define BE_REGISTER_NAMESPACE_3_NAMED(plugin, n1, n2, n3)                                                                                       \
 namespace BugEngine                                                                                                                             \
 {                                                                                                                                               \
-    BE_EXPORT raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3()                                                          \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2();                                                                          \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3()                                                                    \
     {                                                                                                                                           \
         static RTTI::Class ci = { #n3, 0, 0, RTTI::ClassType_Namespace, {be_##plugin##_Namespace_##n1##_##n2().m_ptr}, {0}, {0},                \
                                   {0}, {0, 0}, {0, 0}, {0}, {0}, 0, 0 };                                                                        \
@@ -59,7 +62,8 @@ namespace BugEngine                                                             
 #define BE_REGISTER_NAMESPACE_4_NAMED(plugin, n1, n2, n3, n4)                                                                                   \
 namespace BugEngine                                                                                                                             \
 {                                                                                                                                               \
-    BE_EXPORT raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3##_##n4()                                                   \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3();                                                                   \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3##_##n4()                                                             \
     {                                                                                                                                           \
         static RTTI::Class ci = { #n4, 0, 0, {be_##plugin##_Namespace_##n1##_##n2##_##n3().m_ptr}, {0},                                         \
                                   RTTI::ClassType_Namespace, {0}, {0}, {0, 0}, {0, 0}, {0}, {0}, 0, 0 };                                        \
@@ -80,7 +84,8 @@ namespace BugEngine                                                             
 #define BE_REGISTER_NAMESPACE_5_NAMED(plugin,n1, n2, n3, n4, n5)                                                                                \
 namespace BugEngine                                                                                                                             \
 {                                                                                                                                               \
-    BE_EXPORT raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3##_##n4##_##n5()                                            \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3##_##n4();                                                            \
+    raw<BugEngine::RTTI::Class> be_##plugin##_Namespace_##n1##_##n2##_##n3##_##n4##_##n5()                                                      \
     {                                                                                                                                           \
         static RTTI::Class ci =                                                                                                                 \
             {                                                                                                                                   \
