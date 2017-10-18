3ce81cfe7caf84d86a8059ed22866b1b554ebc5e
yngwe <yngwe@kiff>
2017-10-18 23:59:15 +0200

    fixed usage of windres on Cygwin

--- mak/tools/winres_patch.py
+++ mak/tools/winres_patch.py
@@ -4,7 +4,7 @@ import os
 
 exec_command_winrc_old = winres.winrc.exec_command
 def exec_command_winrc(self, *k, **kw):
-    if os.pathsep == '\\':
+    if os.sep == '\\':
         if isinstance(k[0], list):
             k = ([i.replace('\\', '/') for i in k[0]],) + k[1:]
     return exec_command_winrc_old(self, *k, **kw)
