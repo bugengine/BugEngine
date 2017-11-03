2d31ffb7e5edae83acc4bf35375a78c3d1e14356
yngwe <yngwe@leela>
2017-11-03 09:09:53 +0100

    fixed header guards

--- src/plugin/compute/cpu/include/memorybuffer.hh
+++ src/plugin/compute/cpu/include/memorybuffer.hh
@@ -1,8 +1,8 @@
 /* BugEngine <bugengine.devel@gmail.com> / 2008-2014
    see LICENSE for detail */
 
-#ifndef BE_KERNEL_CPU_MEMORYBUFFER_HH_
-#define BE_KERNEL_CPU_MEMORYBUFFER_HH_
+#ifndef BE_COMPUTE_CPU_MEMORYBUFFER_HH_
+#define BE_COMPUTE_CPU_MEMORYBUFFER_HH_
 /**************************************************************************************************/
 #include    <stdafx.h>
 #include    <scheduler/kernel/imemorybuffer.hh>
--- src/plugin/compute/cpu/include/scheduler.hh
+++ src/plugin/compute/cpu/include/scheduler.hh
@@ -1,8 +1,8 @@
 /* BugEngine <bugengine.devel@gmail.com> / 2008-2014
    see LICENSE for detail */
 
-#ifndef BE_KERNEL_CPU_CPUKERNELSCHEDULER_HH_
-#define BE_KERNEL_CPU_CPUKERNELSCHEDULER_HH_
+#ifndef BE_COMPUTE_CPU_CPUKERNELSCHEDULER_HH_
+#define BE_COMPUTE_CPU_CPUKERNELSCHEDULER_HH_
 /**************************************************************************************************/
 #include    <stdafx.h>
 #include    <scheduler/kernel/ischeduler.hh>
