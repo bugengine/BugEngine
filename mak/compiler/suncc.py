263e43d3bfdf3c1cd11e0a794b449e17bbb1f201
yngwe@FRY <bugengine.devel@gmail.com>
2018-02-18 16:19:45 +0100

    SunCC build fix

--- mak/compiler/suncc.py
+++ mak/compiler/suncc.py
@@ -160,7 +160,7 @@ class SunCC(Configure.ConfigurationContext.GnuCompiler):
         Configure.ConfigurationContext.GnuCompiler.load_in_env(self, conf, platform)
         v = conf.env
         v['RPATH_ST'] = '-R%s'
-        v.CC_CPP = Utils.to_list(env.CC) + ['-E', '-library=no%Cstd']
+        v.CC_CPP = Utils.to_list(v.CC) + ['-E', '-library=no%Cstd']
         if platform.NAME == 'Linux':
             v.IDIRAFTER = '-I'
             #v.STATIC = 1
