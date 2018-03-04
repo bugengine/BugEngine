ea9d6e126dfef81aed8a7f78a24bdd3d33a4ee50
yngwe@bender <yngwe@bender>
2018-03-04 23:39:56 +0100

    Linux: remove unused code

--- mak/host/linux.py
+++ mak/host/linux.py
@@ -1,32 +1,12 @@
 import os
 
 
-def add_ld_so(conf, filename, paths):
-    f = open(filename, 'r')
-    for line in f:
-        line = line.strip()
-        if not line:
-            continue
-        elif line.startswith('#'):
-            continue
-        elif line.startswith('include'):
-            continue
-        elif os.path.isdir(line):
-            paths.append(line)
-
-
 def options(opt):
     pass
 
 
 def configure(conf):
-    lib_paths = []
-    if os.path.isfile('/etc/ld.so.conf'):
-        add_ld_so(conf, '/etc/ld.so.conf', lib_paths)
-    if os.path.isdir('/etc/ld.so.conf.d'):
-        for f in sorted(os.listdir('/etc/ld.so.conf.d')):
-            add_ld_so(conf, '/etc/ld.so.conf.d/'+f, lib_paths)
-    conf.env.ALL_ARCH_LIBPATHS = lib_paths
+    pass
 
 
 def build(bld):
