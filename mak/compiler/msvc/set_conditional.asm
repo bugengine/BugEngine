d6a8bca3d7cfd52e97bcb5b3a76c8fbece608ae5
yngwe@bender <yngwe@bender>
2018-03-26 23:04:44 +0200

    Intel compiler: fixed set_conditional ASM call

--- mak/compiler/msvc/set_conditional.asm
+++ mak/compiler/msvc/set_conditional.asm
@@ -1,14 +1,14 @@
     SECTION .text
     global _set_conditional_128
 _set_conditional_128:
-        mov         rbx,    r10
-        mov         r8,     rax
-        mov         rcx,    r8
-        mov         rdx,    rcx
-        mov         r9,     rbx
-        mov         r9,     rdx
+        mov         r10,    rbx
+        mov         rax,    r8
+        mov         rbx,    r9
+        mov         r8,     rcx
+        mov         rcx,    rdx
+        mov         rdx,    r9
         inc         rbx
         lock cmpxchg16b [r8]
         setz        al
-        mov         r10,    rbx
+        mov         rbx,    r10
         ret
