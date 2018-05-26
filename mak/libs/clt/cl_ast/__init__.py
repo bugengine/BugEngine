302a024f3b6d9f62655a412acf3246434d1d5b7b
yngwe@FRY <bugengine.devel@gmail.com>
2018-05-26 23:19:27 +0200

    Cl: made cl_ast easier to extend

--- mak/libs/clt/cl_ast/__init__.py
+++ mak/libs/clt/cl_ast/__init__.py
@@ -1,7 +1,7 @@
-from .scope import Scope
-from .namespaces import Root, Namespace, AnonymousNamespace
-from .templates import Template
-from .types import Typedef, Typename, Struct, Type, Builtin, Pointer, Reference, Array
-from .methods import Method
-from .variables import Variable
-from .expressions import Specifier, ForStatement
+from .scope import *
+from .namespaces import *
+from .templates import *
+from .types import *
+from .methods import *
+from .variables import *
+from .expressions import *
