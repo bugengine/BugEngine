7cf92c6359f890bbf46d3241c0d9f647c809c7b6
yngwe@FRY <bugengine.devel@gmail.com>
2018-06-23 12:03:11 +0200

    CPP: removed empty files

--- mak/libs/cpp/grammar/__init__.py
+++ mak/libs/cpp/grammar/__init__.py
@@ -2,13 +2,11 @@ from .comment import *
 from .tag import *
 from .name import *
 from .value import *
-from .using import *
 from .type import *
 from .namespace import *
 from .variable import *
 from .struct import *
 from .enum import *
-from .operator import *
 from .method import *
 from .kernel_type import *
 from .exprs import *
