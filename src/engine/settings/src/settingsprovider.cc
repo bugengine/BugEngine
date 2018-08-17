a35dd57b79ff1e62eb5df7249175b8a8355c3df0
yngwe@fry <yngwe@fry>
2018-08-17 16:59:20 +0200

    Settings: implemented command-line settings parsing, Log settings

--- src/engine/settings/src/settingsprovider.cc
+++ src/engine/settings/src/settingsprovider.cc
@@ -28,8 +28,12 @@ void SettingsProvider::addSetting(minitl::hashmap<istring, SettingsList> &contai
     where->second.push_back(minitl::make_tuple(name, value));
 }
 
-SettingsProvider::SettingsProvider(const minitl::hashmap< istring, SettingsList >& initialSettings)
-    :   m_settings(Arena::general(), initialSettings)
+SettingsProvider::SettingsProvider(const ifilename& settingsOrigin,
+                                   const minitl::hashmap< istring, SettingsList >& initialSettings,
+                                   ref<Folder> folder)
+    :   m_filename(settingsOrigin)
+    ,   m_settings(Arena::general(), initialSettings)
+    ,   m_folder(folder)
 {
     SettingsBase::onProviderAdded(this);
 }
@@ -38,8 +42,15 @@ SettingsProvider::~SettingsProvider()
 {
 }
 
-SettingsProvider::SettingsRegistration::SettingsRegistration()
+SettingsProvider::SettingsRegistration::SettingsRegistration(SettingsBase& settings)
 {
+    const minitl::intrusive_list<SettingsProvider>& providers = getSettingsList();
+    for (minitl::intrusive_list<SettingsProvider>::const_iterator it = providers.begin();
+        it != providers.end();
+        ++it)
+    {
+        it->apply(settings);
+    }
 }
 
 SettingsProvider::SettingsRegistration::~SettingsRegistration()
@@ -50,6 +61,49 @@ minitl::intrusive_list<SettingsProvider>& SettingsProvider::SettingsRegistration
 {
     static minitl::intrusive_list<SettingsProvider> s_providerList;
     return s_providerList;
+}
+
+void SettingsProvider::apply(SettingsBase& settings) const
+{
+    RTTI::Type type = RTTI::Type::makeType(settings.m_settingsClass, RTTI::Type::Value,
+                                           RTTI::Type::Mutable, RTTI::Type::Mutable);
+    RTTI::Value settingsValue = RTTI::Value(type , &settings);
+    for (SettingsCategoryMap::const_iterator it = m_settings.begin(); it != m_settings.end(); ++it)
+    {
+        if (it->first == settings.m_settingsClass->name)
+        {
+            for (SettingsList::const_iterator setting = it->second.begin();
+                 setting != it->second.end();
+                 ++setting)
+            {
+                raw<const RTTI::Property> property = settings.m_settingsClass->getProperty(setting->first);
+                if (!property)
+                {
+                    be_error("Unknwon setting %s in category %s" | setting->first | it->first);
+                }
+                else
+                {
+                    RTTI::Parser::DbContext context(Arena::stack(), m_filename, m_folder);
+                    setting->second->resolve(context);
+                    RTTI::Value v = setting->second->eval(context, property->type);
+                    if (!context.errorCount)
+                    {
+                        property->set(settingsValue, v);
+                    }
+                    else
+                    {
+                        for (RTTI::Parser::MessageList::const_iterator message = context.messages.begin();
+                             message != context.messages.end();
+                             ++message)
+                        {
+                            Logger::root()->log(message->severity, m_filename.str(),
+                                                message->location.line, message->message);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 }}
