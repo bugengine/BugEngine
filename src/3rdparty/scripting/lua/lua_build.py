0acdf9c0326fa25d6566f8e3112b33f7220b1fad
yngwe@FRY <bugengine.devel@gmail.com>
2019-03-17 12:47:06 +0100

    fixed Lua/FreeType/Curses/Bullet 3rdparties on Windows

--- src/3rdparty/scripting/lua/lua_build.py
+++ src/3rdparty/scripting/lua/lua_build.py
@@ -4,7 +4,7 @@ from waflib import Options
 def build(bld):
     if not bld.env.SYSTEM_LUA or Options.options.included_lua or bld.env.PROJECTS:
         bld.static_library('3rdparty.scripting.lua', path='.',
-                           extra_defines=['LUA_LIB'],
+                           extra_defines=['LUA_LIB', 'LUA_BUILD_AS_DLL'],
                            extra_includes=[bld.path.make_node('src/core'),
                                            bld.path.make_node('src/lualib')],
                            use_master='folder', warnings=False)
