9c0e32f80482c3f1814b3dd550f8c8b76a114365
yngwe@zoidberg <yngwe@zoidberg>
2019-03-20 17:21:24 +0100

    Also disable rpath for iOS targets

--- extra/iphone/mak/build.py
+++ extra/iphone/mak/build.py
@@ -20,15 +20,12 @@ def set_iphone_shlib_name(self):
         elif 'kernel' in self.features:
             self.env.append_unique('LINKFLAGS', ['-install_name', os.path.join('@executable_path', 'share', 'bugengine', 'kernel', self.link_task.outputs[0].name)])
         else:
-            self.env.append_unique('LINKFLAGS', ['-Wl,-rpath,@loader_path/',
-                                                 '-install_name', os.path.join('@rpath', self.link_task.outputs[0].name)])
+            self.env.append_unique('LINKFLAGS', ['-install_name', os.path.join('@loader_path', self.link_task.outputs[0].name)])
 
 
 @feature('cprogram', 'cxxprogram')
 @after_method('apply_link')
 @after_method('process_use')
 def set_iphone_program_name(self):
-    if 'iphone' in self.env.VALID_PLATFORMS:
-        self.env.append_unique('LINKFLAGS', ['-Wl,-rpath,@loader_path/'])
-
+    pass
 
