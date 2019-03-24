adbcafdfa567479c8c1b7b27f6860007e8f0dcc2
yngwe@fry <yngwe@fry>
2019-03-24 13:31:16 +0100

    iOS/MacOS fixes

--- mak/arch/armv7s.py
+++ mak/arch/armv7s.py
@@ -1,4 +1,4 @@
 def configure(conf):
     conf.recurse('armv7a', once=False)
-    conf.env.VALID_ARCHITECTURES.append('armv7s')
+    conf.env.VALID_ARCHITECTURES = ['armv7s'] + conf.env.VALID_ARCHITECTURES
     conf.env.append_unique('DEFINES', ['_ARM_V7S'])
