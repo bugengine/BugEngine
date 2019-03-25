acb2b767cfaf22e2629db58b409ea45252cba9a6
yngwe@kiff <yngwe@kiff>
2019-03-25 20:59:54 +0100

    SunCC fix

--- src/3rdparty/physics/bullet/src/LinearMath/btConvexHullComputer.cpp
+++ src/3rdparty/physics/bullet/src/LinearMath/btConvexHullComputer.cpp
@@ -19,18 +19,13 @@ subject to the following restrictions:
 #include "btMinMax.h"
 #include "btVector3.h"
 
-#ifdef __GNUC__
+#ifndef _MSC_VER
 	#include <stdint.h>
-#elif defined(_MSC_VER)
+#else
 	typedef __int32 int32_t;
 	typedef __int64 int64_t;
 	typedef unsigned __int32 uint32_t;
 	typedef unsigned __int64 uint64_t;
-#else
-	typedef int int32_t;
-	typedef long long int int64_t;
-	typedef unsigned int uint32_t;
-	typedef unsigned long long int uint64_t;
 #endif
 
 
