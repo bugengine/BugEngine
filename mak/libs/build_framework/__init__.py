e642ccbcded5b86a2641e81f94d909ef48a12f02
yngwe <yngwe@FRY>
2020-01-26 10:50:41 +0100

    fixed build for Python without typing

--- mak/libs/build_framework/__init__.py
+++ mak/libs/build_framework/__init__.py
@@ -284,7 +284,7 @@ class ListVariantsContext(Build.BuildContext):
             print(variant)
 
 
-from typing import TYPE_CHECKING
+from be_typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from waflib import Node
     from typing import Any
