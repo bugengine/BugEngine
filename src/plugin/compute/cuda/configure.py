4aff1bcbea04998198bd15238809d9f83ac03bc3
yngwe@FRY <bugengine.devel@gmail.com>
2020-04-06 18:00:08 +0200

    many CUDA fixes for Windows platforms

--- src/plugin/compute/cuda/configure.py
+++ src/plugin/compute/cuda/configure.py
@@ -21,29 +21,74 @@ def check_nvcc(configuration_context, nvcc):
         except Exception as e:
             pass
         else:
+            if not isinstance(out, str):
+                out = out.decode(sys.stdout.encoding, errors='ignore')
             if not isinstance(err, str):
                 err = err.decode(sys.stderr.encoding, errors='ignore')
             if p.returncode != 0:
                 raise Exception(err)
             else:
                 target_path = None
-                for line in err.split('\n'):
+                for line in out.split('\n') + err.split('\n'):
                     if line.startswith('#$ _TARGET_DIR_='):
                         target_path = line.split('=')[1]
-                if not target_path:
+                        platform = ''
+                    elif line.startswith('#$ _WIN_PLATFORM_='):
+                        target_path = ''
+                        platform = line.split('=')[1]
+                if target_path is None:
                     raise Exception('could not deduce target path')
-                return os.path.join(os.path.dirname(os.path.dirname(nvcc[0])), target_path)
+                return (os.path.join(os.path.dirname(os.path.dirname(nvcc[0])), target_path, 'include'),
+                        os.path.join(os.path.dirname(os.path.dirname(nvcc[0])), target_path, 'lib', platform))
     finally:
         source_node.delete()
 
 
-def configure(configuration_context):
+def find_cuda_registry_paths(all_versions_key):
+    bindirs = []
+    try:
+        root_path, type = Utils.winreg.QueryValueEx(all_versions_key, 'RootInstallDir')
+    except OSError:
+        root_path = ''
+    index = 0
+    while 1:
+        try:
+            version = Utils.winreg.EnumKey(all_versions_key, index)
+            version_key = Utils.winreg.OpenKey(all_versions_key, version)
+        except OSError:
+            break
+        index += 1
+        try:
+            full_path, type = Utils.winreg.QueryValueEx(version_key, 'InstallDir')
+        except OSError:
+            full_path = os.path.join(root_path, version)
+        bindirs.append(os.path.join(full_path, 'bin'))
+    return bindirs
+
+
+def find_cuda_paths(configuration_context):
     compilers = []
-    configuration_context.start_msg('Looking for CUDA')
     v = configuration_context.env
     environ = getattr(configuration_context, 'environ', os.environ)
     bindirs = environ['PATH'].split(os.pathsep) + v.EXTRA_PATH
-    if v.HOST == 'linux':
+    if Utils.winreg:
+        try:
+            all_versions = Utils.winreg.OpenKey(
+                Utils.winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Wow6432node\NVIDIA Corporation\GPU Computing Toolkit\CUDA'
+            )
+        except OSError:
+            pass
+        else:
+            bindirs += find_cuda_registry_paths(all_versions)
+        try:
+            all_versions = Utils.winreg.OpenKey(
+                Utils.winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\NVIDIA Corporation\GPU Computing Toolkit\CUDA'
+            )
+        except OSError:
+            pass
+        else:
+            bindirs += find_cuda_registry_paths(all_versions)
+    else:
         local_dirs = os.listdir('/usr/local')
         for local_dir in local_dirs:
             if local_dir.startswith('cuda'):
@@ -74,7 +119,13 @@ def configure(configuration_context):
                             version = line.split(',')[1].split()[1]
                             version = tuple(int(x) for x in version.split('.'))
                             compilers.append((version, nvcc))
+    return compilers
 
+
+def configure(configuration_context):
+    configuration_context.start_msg('Looking for CUDA')
+    v = configuration_context.env
+    compilers = find_cuda_paths(configuration_context)
     if compilers:
         v.NVCC_COMPILERS = sorted(compilers)
         v.NVCC_CXXFLAGS_debug = ['-D_DEBUG', '--generate-line-info']
@@ -107,7 +158,10 @@ def setup(configuration_context):
             configuration_context.setenv(cuda_toolchain, env=configuration_context.env)
             v = configuration_context.env
             v.NVCC_CXX = compiler
-            v.append_value('NVCC_CXXFLAGS', ['--compiler-bindir', v.CXX[0]])
+            cxx_compiler = v.CXX[0]
+            if v.MSVC_COMPILER:
+                cxx_compiler = cxx_compiler.lower()
+            v.append_value('NVCC_CXXFLAGS', ['--compiler-bindir', cxx_compiler])
             if v.ARCH_LP64:
                 v.append_value('NVCC_CXXFLAGS', ['-m64'])
             else:
@@ -115,7 +169,7 @@ def setup(configuration_context):
             for flag in v.CXXFLAGS:
                 v.append_value('NVCC_CXXFLAGS', ['-Xcompiler', flag])
             try:
-                target_path = configuration_context.check_nvcc(compiler)
+                include_path, lib_path = configuration_context.check_nvcc(compiler)
             except Exception as e:
                 #print(e)
                 #pprint('YELLOW', '-{}'.format(version), sep=' ')
@@ -129,8 +183,8 @@ def setup(configuration_context):
                 #        v.append_value('NVCC_CXXFLAGS_{}'.format(feature), ['-Xcompiler', flag])
 
                 configuration_context.setenv(toolchain)
-                configuration_context.env.append_value('INCLUDES_cuda', [os.path.join(target_path, 'include')])
-                configuration_context.env.append_value('STLIBPATH_cuda', [os.path.join(target_path, 'lib')])
+                configuration_context.env.append_value('INCLUDES_cuda', [include_path])
+                configuration_context.env.append_value('STLIBPATH_cuda', [lib_path])
                 configuration_context.env.append_value('CUDA_VERSIONS', [('cuda{}'.format(version), cuda_toolchain)])
                 configuration_context.env.append_value('FEATURES', ['cuda'])
         if cuda_available:
