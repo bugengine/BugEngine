65714550a8b5096a753a440bce067c77d9affda4
yngwe@FRY <bugengine.devel@gmail.com>
2020-04-06 17:59:39 +0200

    Don't pass path through PYTHONPATH as it breaks on relocatable python

--- mak/bin/kernel_ast.py
+++ mak/bin/kernel_ast.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), 'libs'))
 from cpp import parser, tree
 from optparse import OptionParser
 import traceback
