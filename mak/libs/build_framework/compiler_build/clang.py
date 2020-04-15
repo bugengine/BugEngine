74149e2e3bff1c6004bab81b8f49ce755247517d
yngwe@labarbara <yngwe@labarbara>
2020-04-15 21:19:23 +0200

    fixed Clang command line issues on various platforms; attempt at fixing linking issues with MacOS linkers

--- mak/libs/build_framework/compiler_build/clang.py
+++ mak/libs/build_framework/compiler_build/clang.py
@@ -1,2 +1,29 @@
+import tempfile
+from waflib import Task
+
+
+def clang_exec_command(exec_command):
+    def exec_command_response_file(task, cmd, **kw_args):
+        command = []
+        resp_file_arguments = []
+        inputs = set((x.bldpath() for x in task.inputs))
+        for arg in cmd[1:]:
+            if arg in inputs:
+                resp_file_arguments.append(arg)
+            elif arg[0:2] in ('-I', '-L', '-D'):
+                resp_file_arguments.append(arg)
+            else:
+                command.append(arg)
+        with tempfile.NamedTemporaryFile('w+') as response_file:
+            response_file.write('\n'.join(resp_file_arguments))
+            response_file.flush()
+            return exec_command(task, [cmd[0], '@%s'%response_file.name] + command, **kw_args)
+    return exec_command_response_file
+
+
 def build(bld):
-    pass
+    if bld.env.COMPILER_NAME == 'clang':
+        for cls_name in 'c', 'cxx', 'cshlib', 'cxxshlib', 'cprogram', 'cxxprogram':
+            cls = Task.classes.get(cls_name, None)
+            derived = type(cls_name, (cls, ), {})
+            derived.exec_command = clang_exec_command(derived.exec_command)
