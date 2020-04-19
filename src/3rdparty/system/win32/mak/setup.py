62692c891bc16a23009aeafb3d4efb6da2c7f44f
yngwe@labarbara <yngwe@labarbara>
2020-04-19 21:26:48 +0200

    fixed unistd include path on win32

--- src/3rdparty/system/win32/mak/setup.py
+++ src/3rdparty/system/win32/mak/setup.py
@@ -4,4 +4,4 @@ import os
 def setup(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
         conf.check_lib(['kernel32', 'user32', 'advapi32', 'ole32', 'oleaut32', 'uuid', 'shell32'],
-                       includepath=[os.path.join(conf.path.abspath(), 'api.windows')])
+                       includepath=[os.path.join(conf.path.parent.abspath(), 'api.windows')])
