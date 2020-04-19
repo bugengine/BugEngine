85a1881f51c513c74596d953089eea3f1811a286
yngwe@labarbara <yngwe@labarbara>
2020-04-19 18:09:53 +0200

    new fixes for multiarch, android and iOS

--- mak/libs/build_framework/build/target/target.py
+++ mak/libs/build_framework/build/target/target.py
@@ -4,9 +4,9 @@ import os
 def _recurse(build_context, fun):
     if build_context.env.VALID_PLATFORMS:
         platform = build_context.env.VALID_PLATFORMS[0]
-        try:
+        if os.path.isfile(os.path.join(build_context.path.abspath(), platform + '.py')):
             build_context.recurse(platform + '.py', fun, once=False)
-        except KeyError:
+        else:
             build_context.recurse(os.path.join('extra', platform,), fun, once=False)
 
 
