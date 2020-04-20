4163398fdb1f5d28bea59d5be8d373fe249bd639
yngwe@FRY <bugengine.devel@gmail.com>
2020-04-20 11:25:22 +0200

    fixes for Win32 python detection

--- src/3rdparty/scripting/python27/mak/setup.py
+++ src/3rdparty/scripting/python27/mak/setup.py
@@ -9,12 +9,12 @@ def setup(conf):
 def setup_python(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
         for a in conf.env.VALID_ARCHITECTURES:
-            if os.path.isdir(os.path.join(conf.path.abspath(), 'bin.windows.%s' % a)):
+            if os.path.isdir(os.path.join(conf.path.parent.abspath(), 'bin.windows.%s' % a)):
                 conf.env.check_python27 = True
                 conf.env.check_python27_defines = ['PYTHON_LIBRARY=python27']
                 break
         else:
-            raise Errors.WafError('%s not available for windows %s' % (conf.path.name,
+            raise Errors.WafError('%s not available for windows %s' % (conf.path.parent.name,
                                                                        conf.env.VALID_ARCHITECTURES[0]))
     elif 'macosx' in conf.env.VALID_PLATFORMS:
         conf.env.check_python27 = True
--- src/3rdparty/system/dbghelp/mak/setup.py
+++ src/3rdparty/system/dbghelp/mak/setup.py
@@ -5,8 +5,8 @@ from waflib.Logs import pprint
 def setup(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
         if not conf.check_lib(['psapi', 'dbghelp', 'version'],
-                              includepath=[os.path.join(conf.path.abspath(), 'api')],
-                              libpath=[os.path.join(conf.path.abspath(),
+                              includepath=[os.path.join(conf.path.parent.abspath(), 'api')],
+                              libpath=[os.path.join(conf.path.parent.abspath(),
                               'lib.%s.%s'%(conf.env.VALID_PLATFORMS[0], a)) for a in conf.env.VALID_ARCHITECTURES],
                               includes=['windows.h', 'dbghelp.h'],
                               functions=['SymInitialize']):
