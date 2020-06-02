f503da87fe2fd83249a881e08fa70c8806eabbe0
yngwe@bender <yngwe@bender>
2020-06-02 12:23:51 +0200

    make sure STDAFX is included in settings test project

--- src/test/settings/src/plugin.cc
+++ src/test/settings/src/plugin.cc
@@ -1,6 +1,7 @@
 /* BugEngine <bugengine.devel@gmail.com> / 2008-2014
    see LICENSE for detail */
 
+#include <stdafx.h>
 #include <bugengine/plugin/plugin.hh>
 
 BE_PLUGIN_NAMESPACE_REGISTER();
--- src/test/settings/src/setting.cc
+++ src/test/settings/src/setting.cc
@@ -1,4 +1,5 @@
 /* BugEngine <bugengine.devel@gmail.com> / 2008-2014
    see LICENSE for detail */
 
+#include <stdafx.h>
 #include <setting.script.hh>
