051ecefaf906b3752fb232b19f341db9dcf2a2c9
yngwe@bender <yngwe@bender>
2020-07-21 23:52:58 +0200

    Removed included dbghelp.lib
    Removed the lib file which is actually not necessary, and fixed the setup to make sure it can't fail
    (there's no conditional compilation in the code, so in case psapi/dbghelp is not found the build step would have failed)

--- src/3rdparty/system/dbghelp/mak/setup.py
+++ src/3rdparty/system/dbghelp/mak/setup.py
@@ -5,14 +5,9 @@ def setup(conf):
     if 'windows' in conf.env.VALID_PLATFORMS:
         conf.start_msg_setup()
         if conf.check_lib(
-            ['psapi', 'dbghelp', 'version'],
+            ['psapi', 'version'],
             includepath=[os.path.join(conf.path.parent.abspath(), 'api')],
-            libpath=[
-                os.path.join(conf.path.parent.abspath(), 'lib.%s.%s' % (conf.env.VALID_PLATFORMS[0], a))
-                for a in conf.env.VALID_ARCHITECTURES
-            ],
             includes=['windows.h', 'dbghelp.h'],
-            functions=['SymInitialize']
         ):
             conf.end_msg('from system')
         else:
