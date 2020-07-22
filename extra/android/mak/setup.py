8d4399414510257cc30c18c2cb2e0c7ad1963b65
yngwe@bender <yngwe@bender>
2020-07-22 09:47:32 +0200

    Ported libc++ to the package system.
    libc++ binaries are now hosted on Github and will be downloaded during setup of an Android toolchain.

--- extra/android/mak/setup.py
+++ extra/android/mak/setup.py
@@ -1,5 +1,4 @@
 def setup(configuration_context):
     category_node = configuration_context.path.make_node('../src/3rdparty/android')
     for third_party in category_node.listdir():
-        configuration_context.start_msg('  android.%s' % third_party)
         configuration_context.recurse('../src/3rdparty/android/%s/mak/setup.py' % (third_party), once=False)
