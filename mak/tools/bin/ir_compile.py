90e0c8ace4768be8f54aefd92db496cdaf943893
yngwe@bender <yngwe@bender>
2020-08-16 14:10:40 +0200

    Add shipped PLY path front to avoid conflicts with system PLY

--- mak/tools/bin/ir_compile.py
+++ mak/tools/bin/ir_compile.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0]))), 'libs'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0]))), 'libs'))
 
 if __name__ == '__main__':
     import ircc
