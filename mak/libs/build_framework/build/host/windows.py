07e324834589a6157a2f70e4458db9718351cb7b
yngwe@bender <yngwe@bender>
2020-08-23 13:45:38 +0200

    fix PATH on Windows during build step

--- mak/libs/build_framework/build/host/windows.py
+++ mak/libs/build_framework/build/host/windows.py
@@ -3,6 +3,6 @@ import os
 
 def build(bld):
     environ = getattr(bld, 'environ', os.environ)
-    environ['PATH'] = os.pathsep.join([bld.bugenginenode.make_node("mak/host/win32/bin").abspath(), environ['PATH']])
+    environ['PATH'] = os.pathsep.join([bld.bugenginenode.parent.parent.make_node("mak/host/win32/bin").abspath(), environ['PATH']])
     if bld.env.PATH:
         bld.env.PATH = [bld.bugenginenode.make_node("mak/host/win32/bin").abspath()] + bld.env.PATH
