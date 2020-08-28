a9e316ec8448d0c8ef73af2e2806ed10f69697c6
yngwe@bender <yngwe@bender>
2020-08-28 08:22:09 +0200

    make sure platform.hh is overriden for OpenCL and Cuda

---
+++ src/bugengine/3rdparty/compute/CUDA/api.cuda/bugengine/config/platform.hh
@@ -0,0 +1,11 @@
+/* BugEngine <bugengine.devel@gmail.com> / 2008-2014
+   see LICENSE for detail */
+
+#ifndef BE_CONFIG_PLATFORMS_CUDA_HH_
+#define BE_CONFIG_PLATFORMS_CUDA_HH_
+/**************************************************************************************************/
+
+#define BE_PLATFORM_NAME Cuda
+
+/**************************************************************************************************/
+#endif
--- src/plugin/compute/opencl/api.cl/bugengine/config/platform.hh
+++ src/plugin/compute/opencl/api.cl/bugengine/config/platform.hh
@@ -1,8 +1,8 @@
 /* BugEngine <bugengine.devel@gmail.com> / 2008-2014
    see LICENSE for detail */
 
-#ifndef BE_CORE_PLATFORMS_OPENCL_HH_
-#define BE_CORE_PLATFORMS_OPENCL_HH_
+#ifndef BE_CONFIG_PLATFORMS_OPENCL_HH_
+#define BE_CONFIG_PLATFORMS_OPENCL_HH_
 /**************************************************************************************************/
 
 #define BE_PLATFORM_NAME OpenCL
