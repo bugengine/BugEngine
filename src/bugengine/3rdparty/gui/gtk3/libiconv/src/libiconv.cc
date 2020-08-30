d8608b836600a7827345182696c7b34d85a9692c
yngwe@bender <yngwe@bender>
2020-08-31 00:14:20 +0200

    Extended wchar fix for some old compilers.
    Dragging in cwchar does not force the linker to use the C++ runtime. Drag an actual C++ header to force the link with the C++ runtime.

--- src/bugengine/3rdparty/gui/gtk3/libiconv/src/libiconv.cc
+++ src/bugengine/3rdparty/gui/gtk3/libiconv/src/libiconv.cc
@@ -1,6 +1,8 @@
-/* old MSVC seem to export C functions only in the C++ library. This file will force to link with the C++ library */
 #ifdef _MSC_VER
+/* some wchar functions seem to only be declared for C++. Include cwchar to drag them in */
 #   include <cwchar>
+/* old MSVC seem to export C functions only in the C++ library. This include will force to link with the C++ library */
+#   include <iostream>
 #endif
 
 #include "iconv.c"
