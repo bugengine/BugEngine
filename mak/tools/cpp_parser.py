e77c06018aa8f56f2a6d140057546d1ab6529668
yngwe@scruffy <yngwe@scruffy>
2021-01-01 09:39:15 +0100

    make sure parsers are compiled when starting build

--- mak/tools/cpp_parser.py
+++ mak/tools/cpp_parser.py
@@ -1,5 +1,9 @@
 from cpp import parser as parser_cpp
+from cpprtti import cpp_parser
+from ircc import ir_parser
 
 
 def build(build_context):
     parser_cpp.build_parser(build_context.bldnode.parent.parent.make_node('cpp_grammar.pickle').abspath())
+    cpp_parser.CppParser(build_context.bldnode.parent.parent.abspath())
+    ir_parser.IrParser(build_context.bldnode.parent.parent.abspath())
