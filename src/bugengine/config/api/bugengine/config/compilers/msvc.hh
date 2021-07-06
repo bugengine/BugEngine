8a81c8b6e9f1be1a16a20c2e668cede74bd2addf
yngwe@fry <yngwe@fry>
2021-07-06 23:33:56 +0200

    msvc: fix warning on old compiler

--- src/bugengine/config/api/bugengine/config/compilers/msvc.hh
+++ src/bugengine/config/api/bugengine/config/compilers/msvc.hh
@@ -40,44 +40,62 @@ typedef unsigned __int32 u32;
 typedef unsigned __int64 u64;
 typedef u8               byte;
 
-#pragma warning(                                                                                   \
-    default : 4263)  // member function does not override any base class virtual member function
-#pragma warning(default : 4264)  // no override available for virtual member function from base
-                                 // 'class'; function is hidden
-#pragma warning(default : 4265)  // class has virtual functions, but destructor is not virtual
-#pragma warning(default : 4266)  // no override available for virtual member function from base
-                                 // 'type'; function is hidden
-#pragma warning(default : 4906)  // wide string literal cast to 'LPSTR'
-#pragma warning(default : 4946)  // reinterpret_cast used between related classes
-
-#pragma warning(error : 4715)  // not all control paths return a value
-
-#pragma warning(                                                                                   \
-    disable : 4251)  // class 'X' needs to have dll-interface to be used by clients of class 'Y'
-#pragma warning(                                                                                   \
-    disable : 4275)  // non dll-interface class 'X' used as base for dll-interface class 'Y'
-#ifdef NDEBUG
-#    pragma warning(error : 4541)  // 'dynamic_cast' used on polymorphic type with '/GR-'
-#    pragma warning(                                                                               \
-        disable : 4530)  // C++ exception handler used, but unwind semantics are not enabled
-#    pragma warning(disable : 4100)  // unreferenced formal parameter
-#    pragma warning(disable : 4702)  // unreachable code
+// member function does not override any base class virtual member function
+#pragma warning(default : 4263)
+// no override available for virtual member function from base 'class'; function is hidden
+#pragma warning(default : 4264)
+// class has virtual functions, but destructor is not virtual
+#pragma warning(default : 4265)
+// no override available for virtual member function from base'type'; function is hidden
+#if(_MSC_VER < 1400)
+#    pragma warning(disable : 4266)
+#else
+#    pragma warning(default : 4266)
 #endif
-#pragma warning(                                                                                   \
-    disable : 4351)  // new behavior: elements of array '...' will be default initialized
-#pragma warning(disable : 4355)  // this used in base member initialization list
-#pragma warning(disable : 4127)  // conditional expression is constant
-//#pragma warning(disable:4181)   // qualifier applied to reference type; ignored
-#pragma warning(disable : 4505)  // unreferenced local function has been removed
-#pragma warning(disable : 4510)  // default constructor could not be generated
-#pragma warning(disable : 4511)  // copy constructor could not be generated
-#pragma warning(disable : 4512)  // assignment operator could not be generated
-#pragma warning(                                                                                   \
-    disable : 4610)  // struct X can never be instantiated - user defined constructor required
-#pragma warning(disable : 4800)  // forcing value to bool 'true' or 'false' (performance warning)
-#pragma warning(                                                                                   \
-    disable : 4359)  // Alignment specifier is less than actual alignment (X) and will be ignored
-#pragma warning(disable : 4324)  // structure was padded due to alignment specifier
+// wide string literal cast to 'LPSTR'
+#pragma warning(default : 4906)
+// reinterpret_cast used between related classes
+#pragma warning(default : 4946)
+// not all control paths return a value
+#pragma warning(error : 4715)
+
+// class 'X' needs to have dll-interface to be used by clients of class 'Y'
+#pragma warning(disable : 4251)
+// non dll-interface class 'X' used as base for dll-interface class 'Y'
+#pragma warning(disable : 4275)
+#ifdef NDEBUG
+// 'dynamic_cast' used on polymorphic type with '/GR-'
+#    pragma warning(error : 4541)
+// C++ exception handler used, but unwind semantics are not enabled
+#    pragma warning(disable : 4530)
+// unreferenced formal parameter
+#    pragma warning(disable : 4100)
+// unreachable code
+#    pragma warning(disable : 4702)
+#endif
+// new behavior: elements of array '...' will be default initialized
+#pragma warning(disable : 4351)
+// this used in base member initialization list
+#pragma warning(disable : 4355)
+// conditional expression is constant
+#pragma warning(disable : 4127)
+// unreferenced local function has been removed
+#pragma warning(disable : 4505)
+// default constructor could not be generated
+#pragma warning(disable : 4510)
+// copy constructor could not be generated
+#pragma warning(disable : 4511)
+// assignment operator could not be generated
+#pragma warning(disable : 4512)
+// struct X can never be instantiated - user defined constructor required
+#pragma warning(disable : 4610)
+// forcing value to bool 'true' or 'false' (performance warning)
+#pragma warning(disable : 4800)
+// Alignment specifier is less than actual alignment (X) and will be ignored
+#pragma warning(disable : 4359)
+// structure was padded due to alignment specifier
+#pragma warning(disable : 4324)
+
 #define BE_NOINLINE     __declspec(noinline)
 #define BE_ALWAYSINLINE __forceinline
 #ifdef _CPPUNWIND
@@ -108,12 +126,16 @@ typedef u8               byte;
 #endif
 
 #if _MSC_VER >= 1400
-#    pragma warning(disable : 4481)  // use of "override" extension
+// use of "override" extension
+#    pragma warning(disable : 4481)
 #else
 #    define override
-#    pragma warning(disable : 4702)  // unreachable code
-#    pragma warning(disable : 4714)  // function marked as __forceinline but not inlined
+// unreachable code
+#    pragma warning(disable : 4702)
+// function marked as __forceinline but not inlined
+#    pragma warning(disable : 4714)
 #endif
+
 #define _CRT_SECURE_NO_DEPRECATE 1
 
 /**************************************************************************************************/
